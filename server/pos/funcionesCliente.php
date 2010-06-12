8aba96652fc9d9a1d1d678314dd121b8a9e0954d
Juan Manuel Hernandez <figu05@gmail.com>
2010-06-12 20:06:28 +0000

    scrips que se conectan con la interfaz

---
+++ server/pos/funcionesCliente.php
@@ -0,0 +1,82 @@
+﻿<?php
+	include("../AddAllClass.php");
+
+	function insertar(){
+		$id =null;
+		$rfc=$_REQUEST['rfc'];
+		$nombre=$_REQUEST['nombre'];
+		$direccion=$_REQUEST['direccion'];
+		$telefono=$_REQUEST['telefono'];
+		$e_mail=$_REQUEST['e_mail'];
+		$limite_credito=$_REQUEST['limite_credito'];
+		$cliente = new cliente($rfc,$nombre,$direccion,$telefono,$e_mail,$limite_credito);
+		
+		if ($cliente->inserta()){
+			echo "{success: true }";
+		}else{
+			echo "{success: false }";
+		}
+	}
+	function modificar(){
+		
+		$cliente = new cliente_existente($_REQUEST['id']); 
+		$cliente->id_cliente=$_REQUEST['id'];
+		$cliente->rfc=$_REQUEST['rfc'];
+		$cliente->nombre=$_REQUEST['nombre'];
+		$cliente->direccion=$_REQUEST['direccion'];
+		$cliente->telefono=$_REQUEST['telefono'];
+		$cliente->e_mail=$_REQUEST['e_mail'];
+		$cliente->limite_credito=$_REQUEST['limite_credito'];
+	
+		
+		if($cliente->actualiza()){
+			echo "{success: true}";
+		}else{
+			echo "{success: false}";
+		}
+	}
+	
+	function eliminar(){
+		$cliente = new cliente_existente($_REQUEST['id']); 
+		$cliente->id_cliente =$_REQUEST['id'];
+		if($cliente->borra()){
+			echo "{success: true }";
+		}else{
+			echo "{success : false }";
+		}
+	}
+	
+	function listarClientes(){
+		$listar = new listar("select * from cliente",array());
+		echo $listar->lista();
+		return $listar->lista();
+	}
+	
+	function mostrarCliente(){
+		$id=$_REQUEST['id'];
+		//$cliente = new listar("select * from cliente where id_cliente=".$id.";",array());
+		$cliente = new cliente_existente($id);
+		$cliente->id_cliente=$id;
+		echo "{ success: true , \"datos\":".$cliente->json()."}";
+		//echo "".$cliente->lista();
+	}
+	
+	switch ($_REQUEST['action']){
+	case 'list':
+		listarClientes();
+	break;
+	case 'update':
+		modificar();
+	break;
+	case 'delete':
+		eliminar();
+	break;
+	case 'insert': 
+		insertar();
+	break;
+	case 'showClient':
+		mostrarCliente();
+	break;
+}//fin switch
+	
+?>
---
+++ server/pos/funcionesProveedor.php
@@ -0,0 +1,79 @@
+﻿<?php
+	include("../AddAllClass.php");
+
+	function insertar(){
+		$id =null;
+		$rfc=$_REQUEST['rfcP'];
+		$nombre=$_REQUEST['nombreP'];
+		$direccion=$_REQUEST['direccionP'];
+		$telefono=$_REQUEST['telefonoP'];
+		$e_mail=$_REQUEST['e_mailP'];
+
+		$proveedor = new proveedor($rfc,$nombre,$direccion,$telefono,$e_mail);
+		
+		if ($proveedor->inserta()){
+			echo "{success: true }";
+		}else{
+			echo "{success: false }";
+		}
+	}
+	function modificar(){
+		
+		$proveedor = new proveedor_existente($_REQUEST['idP']); 
+		$proveedor->id_proveedor=$_REQUEST['idP'];
+		$proveedor->rfc=$_REQUEST['rfcP'];
+		$proveedor->nombre=$_REQUEST['nombreP'];
+		$proveedor->direccion=$_REQUEST['direccionP'];
+		$proveedor->telefono=$_REQUEST['telefonoP'];
+		$proveedor->e_mail=$_REQUEST['e_mailP'];
+	
+		
+		if($proveedor->actualiza()){
+			echo "{success: true}";
+		}else{
+			echo "{success: false}";
+		}
+	}
+	
+	function eliminar(){
+		$proveedor = new proveedor_existente($_REQUEST['idP']); 
+		$proveedor->id_proveedor =$_REQUEST['idP'];
+		if($proveedor->borra()){
+			echo "{success: true }";
+		}else{
+			echo "{success : false }";
+		}
+	}
+	
+	function listarProveedores(){
+		$listar = new listar("select * from proveedor",array());
+		echo $listar->lista();
+		return $listar->lista();
+	}
+	
+	function mostrarProveedor(){
+		$id=$_REQUEST['idP'];
+		$proveedor = new proveedor_existente($id);
+		$proveedor->id_proveedor=$id;
+		echo "{ success: true , \"datos\":".$proveedor->json()."}";
+	}
+	
+	switch ($_REQUEST['action']){
+	case 'list':
+		listarProveedores();
+	break;
+	case 'update':
+		modificar();
+	break;
+	case 'delete':
+		eliminar();
+	break;
+	case 'insert': 
+		insertar();
+	break;
+	case 'showProveedor':
+		mostrarProveedor();
+	break;
+}//fin switch
+	
+?>
