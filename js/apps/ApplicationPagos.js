88bac1192beb55c3fb05e00b3c66d2a85d043b48
Diego Ventura <dventuras11@gmail.com>
2010-06-23 10:51:01 +0000

    Se agrego la aplicacion de pagos aun sin funcionar

---
+++ js/apps/ApplicationPagos.js
@@ -0,0 +1,150 @@
+ApplicationPagos = function ()
+{
+	if(DEBUG){
+		console.log("ApplicationPagos: construyendo");
+	}
+	return this._init();
+};
+
+
+//variables de esta clase, NO USEN VARIABLES ESTATICAS A MENOS QUE SEA 100% NECESARIO
+
+//aqui va el panel principal 
+ApplicationPagos.prototype.mainCard = null;
+
+//aqui va el nombre de la applicacion
+ApplicationPagos.prototype.appName = null;
+
+//aqui va el nombre de la applicacion
+ApplicationPagos.prototype.leftMenuItems = null;
+
+//aqui va un texto de ayuda en html
+ApplicationPagos.prototype.ayuda = null;
+
+
+ApplicationPagos.prototype.date;
+ApplicationPagos.prototype.clientesStore;
+
+ApplicationPagos.prototype._init = function()
+{
+	
+	//iniciar variables
+	
+	//nombre de la aplicacion
+	this.appName = "Pagos";
+	
+	//ayuda sobre esta applicacion
+	this.ayuda = "ayuda sobre este modulo de Pagos";
+	
+	//submenues en el panel de la izquierda
+	this.leftMenuItems = [
+	{
+        text: 'Listar',
+       	card: this.listar,
+        ayuda: 'ayuda en listado'
+    },
+    {
+        text: 'Abonar',
+       	card: this.abonar,
+        ayuda: 'ayuda de abonar'
+    }
+	];
+	
+	//panel principal
+	this.mainCard = new Ext.Panel({
+		cls: 'card card1',
+		html: 'PAGOS'
+	});
+	
+	POS.AJAXandDECODE({
+			method: 'listarClientes'
+		}, 
+	function (d){
+		this.date=d;
+		if(date.success){
+			this.clientesStore=new Ext.data.Store({
+				fields: ['id_cliente','rfc','nombre','direccion','telefono','e_mail','limite_credito'],
+				idIndex:'id_cliente',
+				data:this.date
+			});
+		}
+	},
+	function (){
+	}
+	);
+
+};
+
+
+
+ApplicationPagos.prototype.listar = new Ext.Panel({
+    scroll: 'vertical',
+	cls: 'cards',
+	layout: {
+		type: 'vbox',
+		align: 'top'
+	},
+	defaults:{
+		flex: 1
+	},
+	items:[
+		new Ext.form.FormPanel({
+			items: [{
+					xtype: 'fieldset',
+					title: 'Filtrar por:',
+					defaults: {
+						xtype: 'radio'
+					},
+					items: [{
+						name: 'filtro',
+						label: 'todos'
+					},
+					{
+						name: 'filtro',
+						label: 'Adeudan'
+					},
+			{
+                fieldLabel: 'nombre',
+                name: 'nombre',
+				xtype : "combo",
+				store:this.clientesStore,
+				displayField:'nombre',
+				typeAhead: true,
+				mode: 'local',
+				forceSelection: true,
+				triggerAction: 'all',
+				emptyText:'Seleccione Autor',
+                allowBlank:false
+            },
+					{
+						name: 'filtro',
+						label: 'Pagados'
+					}]
+				},
+				{
+					fieldLabel: 'Cliente:',
+					xtype : "textfield",
+					name: 'nombre:',
+					allowBlank:false
+				}
+			]
+		}),
+		{
+			html: 'grid con los datos',
+			cls: 'card3'
+		}
+		]
+});
+
+ApplicationPagos.prototype.abonar = new Ext.form.FormPanel({
+    scroll: 'vertical',
+    items: [{
+        xtype: 'slider',
+        name: 'Fotmulario',
+        label: 'form:'
+    }]
+});
+
+
+//autoinstalar esta applicacion
+AppInstaller( new ApplicationPagos() );
