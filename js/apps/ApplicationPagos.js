1e87967778c428770b96d45d9a3de706dd178762
Diego Ventura <dventuras11@gmail.com>
2010-07-02 22:17:16 +0000

    Se agregaron comentarios a la aplicacion de pagos, solo falta seleccionar el cliente y eliminar el bug de la pantalla de pagos

--- js/apps/ApplicationPagos.js
+++ js/apps/ApplicationPagos.js
@@ -28,22 +28,33 @@ ApplicationPagos.prototype.leftMenuItems = null;
 //aqui va un texto de ayuda en html
 ApplicationPagos.prototype.ayuda = null;
 
-ApplicationPagos.prototype.clientesContainer= null;
+//store en el que tendra los datos de todas las compras a credito
+ApplicationPagos.storeVentasCredito = null;
 
-ApplicationPagos.storeVentas = null;
-
+//datos de las ventas a credito del AJAXANDDECODE
 ApplicationPagos.prototype.datosVentas = null;
+//datos de los pagos de las ventas a credito del AJAXANDDECODE
 ApplicationPagos.prototype.datosPagos = null;
+//formulario de las fechas a seleccionar
 ApplicationPagos.fechas = null;
+//boton para seleccionar un cliente
 ApplicationPagos.btnClientes = null;
+//formualario en donde se colocaran los botones de cliente y fechas
 ApplicationPagos.formulario = null;
+//funcion que llamarala ventana parapagar las ventas
 ApplicationPagos.prototype.PagarVenta=null;
 
+//variables que se ocupan para la busqueda de ventas a credito
+//id de cliente a buscar
 ApplicationPagos.idCliente = null;
+//fecha de inicio a buscar
 ApplicationPagos.fechaInicio = new Date();
+//fecha hasta la que se buscara
 ApplicationPagos.fechaFin = new Date();
+//tipo de busqueda, 1=adeudan, 2=pagados, 3=todos
 ApplicationPagos.tipo = 1;
 
+//constructor.
 ApplicationPagos.prototype._init = function()
 {
 	
@@ -57,7 +68,9 @@ ApplicationPagos.prototype._init = function()
 	this.ayuda = "Ayuda sobre este modulo de prueba <br>, html es valido <br> :D";
 	
 	//submenues en el panel de la izquierda
+	//no agrega ninguno
 	
+	//agregamos el toolbar
 	this._initToolBar();
 	
 };//fin CONSTRUCTOR
@@ -67,14 +80,13 @@ ApplicationPagos.prototype._init = function()
 //Iniciar el toolbar
 ApplicationPagos.prototype._initToolBar = function (){
 	
-	//Dejo los demas grupos por si quieres agregar mas botones
-	//grupo 1, funciones basicas
+	//primer grupo de botones, solo el boton de buscar
 	var buttonsGroup1 = [{
         text: 'Buscar',
         ui: 'round',
         handler: clickBuscar
     }];
-	//grupo 2, cualquier otra mamada
+	//grupo 2 la busqueda por estado de credito
 	var buttonsGroup2 = [{
         xtype: 'splitbutton',
         activeButton: 0,
@@ -92,7 +104,7 @@ ApplicationPagos.prototype._initToolBar = function (){
             handler: clickTodos
         }]    
     }];
-	//grupo 3, listo para vender
+	//grupo 3 activar busqueda por cliente y/o periodo
     var buttonsGroup3 = [{
         text: 'Cliente',
         handler: clickCliente
@@ -100,7 +112,7 @@ ApplicationPagos.prototype._initToolBar = function (){
         text: 'Periodo',
         handler: clickPeriodo
     }];
-
+	//agregamos los grupos de botones a la tool bar
 	if (!Ext.platform.isPhone) {
         buttonsGroup1.push({xtype: 'spacer'});
         buttonsGroup2.push({xtype: 'spacer'});
@@ -132,13 +144,15 @@ ApplicationPagos.prototype._initToolBar = function (){
             dock: 'bottom'
         }];
     }
-	//agregar este dock a el panel que quieras
+	//agregar este dock a el panel principal
 	this.mainCard.addDocked( this.dockedItems );
 };
-
+//fin del toolbar
 		
-		
-//Funciones para los pickers.		
+//-------------------------------------------------------------------------------------
+//								Funciones para los pickers.
+//-------------------------------------------------------------------------------------
+//picker para la fecha de inicio
 this.clickFechaInicio=function(){
 
     var fechaActual = new Date();
@@ -179,7 +193,9 @@ this.clickFechaInicio=function(){
         });
         picker.show();
 };
-	
+//picker fecha inicio
+
+//picker fin de periodo
 this.clickfechaFin=function(){
     var fechaActual = new Date();
     var dia = ApplicationPagos.fechaFin.getDate();
@@ -217,7 +233,13 @@ this.clickfechaFin=function(){
         picker.show();
 
 };
+//picker fecha fin
 
+//--------------------------------------------------------------------------------
+//					Inicializamos los controles del formulario
+//--------------------------------------------------------------------------------
+
+//Boton de clientes
 ApplicationPagos.btnClientes=new Ext.Button({
 			hidden:'true',		
 			handler:function (){
@@ -227,7 +249,9 @@ ApplicationPagos.btnClientes=new Ext.Button({
 			width: 300,
             text : 'Seleccionar Cliente'
         });	
-		
+//btnClientes		
+
+//formulario de las fechas
 ApplicationPagos.fechas = new Ext.form.FormPanel({
 	hidden:'true',
     items: [
@@ -256,10 +280,9 @@ ApplicationPagos.fechas = new Ext.form.FormPanel({
 		id : 'ffin'
 	}]
 });
+//fechas
 
-
-
-
+//agregamos los controles al formulario
 ApplicationPagos.formulario=new Ext.form.FormPanel({
 	minHeight:80,
     items: [{
@@ -269,42 +292,63 @@ ApplicationPagos.formulario=new Ext.form.FormPanel({
         items: [	ApplicationPagos.btnClientes,ApplicationPagos.fechas	]
     }]
 });
+//formulario
 
+//------------------------------------------------------------------------------------------
+//							Inicializamos el store de ventas a credito
+//------------------------------------------------------------------------------------------
 
+//agregamos el modelo para ver las ventas a credito
 Ext.regModel('modeloVentas', {
     fields: ['id_venta','total', 'pagado', 'debe', 'nombre', 'fecha']
 });
 
-
-
-ApplicationPagos.storeVentas = new Ext.data.Store({
+//inicializamos el store de ventas a credito
+ApplicationPagos.storeVentasCredito = new Ext.data.Store({
     model: 'modeloVentas',
     sorters: 'fecha',
     getGroupString : function(record) {
         return record.get('nombre')[0];
     }
 });
+//store ventas credito
 
 
+//------------------------------------------------------------------------------------------
+//					Funciones AjaxAndDecode para los datos de los stores
+//------------------------------------------------------------------------------------------
 
-ApplicationPagos.funcion_ajax_ventas = function(cliente,deFecha,aFecha){
+//ajax and decode para la busqueda de ventas a credito, funciona en todos los casos de busqueda
+ApplicationPagos.funcion_ajax_ventas_credito = function(cliente,deFecha,aFecha){
+//	-cliente el el id de cliente a buscar
+//	-defecha es el inicio de la fecha busqueda
+//	-afecha el el fin de la fecha de busqueda
+
+//El tipo nos da si van ser adeudados, pagados o todos
 tipo=ApplicationPagos.tipo;
+
+//segun el tipo elejimos el metodo que enviaremos al ajaxAndDecode
 var metodo=(tipo==1)?'reporteClientesComprasCreditoDeben':((tipo==2)?'reporteClientesComprasCreditoPagado':'reporteClientesComprasCredito');
 						
-						if(DEBUG){console.log("llamando funcion_ajax_ventas");}
-//							POS.aviso("datos",cliente+"--"+deFecha+"--"+aFecha);
+						if(DEBUG){console.log("llamando funcion_ajax_ventas_credito");}
 						POS.AJAXandDECODE({
+							//Parametros a enviar
 							method: metodo,
+							//id del cliente a buscar, si es null nos devuelve todos
 							id_cliente: cliente,
+							//fechas del periodo, si son null muestra todos
 							de: deFecha,
 							al: aFecha
 							},
 							function (datos){
 								if(datos.success){
+									//el resultado es exitoso
 									this.datosVentas = datos.datos;
-									ApplicationPagos.storeVentas.loadData(this.datosVentas); 
+									//cargamos los datos al store
+									ApplicationPagos.storeVentasCredito.loadData(this.datosVentas); 
 								}else{
-									ApplicationPagos.storeVentas.loadData(new Array()); 
+									//limpia el store si no encuentra datos
+									ApplicationPagos.storeVentasCredito.loadData(new Array()); 
 								}
 							},
 							function (){//no responde
@@ -312,32 +356,43 @@ var metodo=(tipo==1)?'reporteClientesComprasCreditoDeben':((tipo==2)?'reporteCli
 							}
 						);	//ajaxAndDecode
 };
+//funcion_ajax_ventas_credito
 
+//ajaxAndDecode para los pagos de la venta
 ApplicationPagos.funcion_ajax_pagos = function(id,debe){
 
-Ext.regModel('modeloPagos', {
-    fields: ['id_pago','id_venta','fecha','monto']
-});
+	//registramos el modelo de los pagos de la venta
+	Ext.regModel('modeloPagos', {
+		fields: ['id_pago','id_venta','fecha','monto']
+	});
 
-
-var storePagos = new Ext.data.Store({
-    model: 'modeloPagos',
-    sorters: 'fecha',
-    getGroupString : function(record) {
-        return record.get('fecha')[0];
-    }
-});
+	//definimos el store
+	var storePagos = new Ext.data.Store({
+		model: 'modeloPagos',
+		sorters: 'fecha',
+		getGroupString : function(record) {
+			return record.get('fecha')[0];
+		}
+	});
+	
+	//llamamos ajaxAndDecode para asignar los datos al store
 	POS.AJAXandDECODE({
 		method: 'listarPagosVentaDeVenta',
+		//mandamos el id de la venta para la que buscamos los datos
 		id_venta:id
 		},
+		//peticion correcta
 		function (datos){
 			if(datos.success){
+					//asignamos datos al store
 					this.datosPagos = datos.datos;
 					storePagos.loadData(this.datosPagos); 
 			}else{
+				//si el success es false el store queda vacio
+				//si nos regresa alguna razon de por que falla nos manda la razon
 				if(datos.reason!==undefined){		POS.aviso("error",datos.reason);	}
 			}
+			//llamamos a la ventana que nos muestra los pagos, le enviamos el store para que aparezca hasta que este tenga datos
 			ApplicationPagos.muestraPagos(id,debe,storePagos);
 		},
 		function (){//no responde
@@ -345,25 +400,32 @@ var storePagos = new Ext.data.Store({
 		}
 	);	//ajaxAndDecode
 };
+//funcion_ajax_pagos
 
+//----------------------------------------------------------------------------------
+//						Formularios dinamicos eventos
+//----------------------------------------------------------------------------------
 
-
-
+//formulario para pagar la venta a credito
 ApplicationPagos.prototype.PagarVenta=function(id,debe){	
+		//el numberfield en el que pondremos la cantidad a abonar
 		var montoPago=new  Ext.form.NumberField({
                         name: 'monto',
                         label: 'Cantidad',
 						id: 'montoPago'
                     });
-		var formPagaBase =  {
+		//formulario en el que va el numberfield
+		formPagaBase =  {
             scroll: 'vertical',
             items: [
                 {
                     xtype: 'fieldset',
                     title: 'Pagar venta '+id+" adeuda: "+debe,
+					//agregamos el boton al formulario
                     items: [montoPago]
                 }
             ],
+			//agregamos el toolbar de la ventana, cerrar y pagar
 			dockedItems: [{
                     xtype: 'toolbar',
                     dock: 'bottom',
@@ -372,41 +434,68 @@ ApplicationPagos.prototype.PagarVenta=function(id,debe){
 						text: 'Cerrar',
 						handler: function() {
 							//ocultar esta tabla
-							form.hide();	
-							form.destroy();	
+							formPago.hide();	
+							formPago.destroy();	
                             }
 					},{
 						xtype:'button',
 						text: 'Guardar',
 						handler: function() {	
+							//patron para verificar si es un valor valido
 							var patron=/^\d*.\d{0,2}$/;
+							//verificamos que la cantidad sea valida
 							if (patron.test(montoPago.getValue())){
+							//cantidad valida
+								//ajax and decode que guarda el pago
 								POS.AJAXandDECODE({
+									//mandamos el id de la venta y la cantidad
 									method: 'insertarPagoVenta',
 									id_venta: id,
 									monto: montoPago.getValue()
 									},
 									function (datos){
+									//peticion exitosa
 										if(datos.success){
-											if(datos.cambio===undefined){POS.aviso("Guardado","Pago guardado correctamente");}
-											else{POS.aviso("Guardado","Pago guardado correctamente. Cambio: $"+datos.cambio);}
-											ApplicationPagos.funcion_ajax_ventas(null,null,null);
-										}else{
+											//nos envia un success true
+											if(datos.cambio===undefined)
+											{
+												//si no nos regresa cambio
+												POS.aviso("Guardado","Pago guardado correctamente");
+											}
+											else
+											{
+												//si tenemos que devolver cambio
+												POS.aviso("Guardado","Pago guardado correctamente. Cambio: $"+datos.cambio);
+											}
+											//actualizamos la lista de ventas a credito
+											ApplicationPagos.funcion_ajax_ventas_credito(null,null,null);
+										}
+										else
+										{
+											//nos devuelve un succes false
 											POS.aviso("Vacio.",datos.reason);
 										}
 									},
-									function (){//no responde
+									function (){
+										//no responde la peticion
 										POS.aviso("ERROR","Error al guardar los datos");
 									}
 								);	//ajaxAndDecode
-								form.hide();	
-								form.destroy();
-							}else {POS.aviso("ERROR","No inserto una cantidad valida");}
-						}
-					}]
-                }
-            ]
+								formPago.hide();	
+								formPago.destroy();
+							}
+							else 
+							{
+								//cantidad invalida
+								POS.aviso("ERROR","No inserto una cantidad valida");
+							}
+						}//handler boton pagar
+					}]//items del toolbar
+                }//toolbar
+            ]//items dockedItems
 		};//fromulario
+		
+		//definimos como se va a mostrar elformulario
         if (Ext.platform.isPhone) {
             formPagaBase.fullscreen = true;
         } else {
@@ -420,14 +509,17 @@ ApplicationPagos.prototype.PagarVenta=function(id,debe){
                 width: 400
             });
         }
-        var form = new Ext.form.FormPanel(formPagaBase);
-        form.show();
+		//asignamos el formulario anterior a al formpago
+        var formPago = new Ext.form.FormPanel(formPagaBase);
+		
+		//mostramos el formulario
+        formPago.show();
 };//PagarVenta
 
 
 
-
-ApplicationPagos.muestraPagos=function(id,debe,store){		
+//formulario para mostrar los pagos de ventas a credito
+ApplicationPagos.muestraPagos=function(id,debe,store){	
 							
 		var formBase = {
 			//	items
@@ -447,7 +539,8 @@ ApplicationPagos.muestraPagos=function(id,debe,store){
 		        itemSelector: 'div.pagos',
 		        singleSelect: true,
 		        indexBar: true
-		    }],
+		    }]//items
+			,
 			dockedItems: [{
                     xtype: 'toolbar',
                     dock: 'bottom',
@@ -480,11 +573,12 @@ ApplicationPagos.muestraPagos=function(id,debe,store){
 									Ext.getCmp('ListaPagos').store = null;
 									Ext.getCmp('ListaPagos').destroy();
 								}
+								//llama formulario para pagar
 								ApplicationPagos.currentInstance.PagarVenta(id,debe);
-                            }
-					}]
-                }
-            ]
+                            }//handler boton pagar
+					}//boton pagar
+					]//items toolbar
+                }]//docked
 		};//fromulario
         if (Ext.platform.isPhone) {
             formBase.fullscreen = true;
@@ -504,6 +598,14 @@ ApplicationPagos.muestraPagos=function(id,debe,store){
 };//muestraPagos
 
 
+
+
+//--------------------------------------------------------------------------------------------------
+//				PANEL PRINCIPAL (MAINCARD)
+//--------------------------------------------------------------------------------------------------
+
+
+//Panel Principal
 ApplicationPagos.prototype.mainCard = new Ext.Panel({
    
     scroll: 'vertical',
@@ -516,7 +618,7 @@ ApplicationPagos.prototype.mainCard = new Ext.Panel({
 	
     listeners: {
 		beforeshow : function(component){
-						ApplicationPagos.funcion_ajax_ventas(null,null,null);
+						ApplicationPagos.funcion_ajax_ventas_credito(null,null,null);
 		}//fin before
 	},
     items: [ApplicationPagos.formulario,
@@ -530,7 +632,7 @@ ApplicationPagos.prototype.mainCard = new Ext.Panel({
 				height: '100%',
 				xtype: 'list',
 				id:'listaVentas',
-				store: ApplicationPagos.storeVentas,
+				store: ApplicationPagos.storeVentasCredito,
 				tpl: 	'<tpl for="."><div class="modeloVentas"><pre>{id_venta}		{total}		{pagado} 		{debe} 		{nombre} 		   {fecha}</pre></div></tpl>',
 				itemSelector: 'div.modeloVentas',
 				singleSelect: true,
@@ -551,48 +653,69 @@ ApplicationPagos.prototype.mainCard = new Ext.Panel({
 			}
 			]//items principal	
 });
+//fin de panel principal
 
+//--------------------------------------------------------------------------
+//									Eventos botones
+//--------------------------------------------------------------------------
 
-
+//clickBuscar
 this.clickBuscar = function ()
 {
+	//obtiene las fechas del periodo
 	var de=Ext.get("finicio").dom.textContent;
 	var al=Ext.get("ffin").dom.textContent;
+	//obtiene el id del cliente
 	var cliente=ApplicationPagos.idCliente;
+	
+	//verifica el caso, es decir si se busca por periodo, fecha, ambos o ninguno y manda los parametros a la carga de la lista
 	if(!ApplicationPagos.btnClientes.isVisible()){
 			if(!(ApplicationPagos.fechas.isVisible())){
-				POS.aviso("Buscar","Buscar al cliente: "+cliente+" del:"+de+" al "+al); 				
+				ApplicationPagos.funcion_ajax_ventas_credito(cliente,de,al);				
 			}else{
-				POS.aviso("Buscar","Buscar al cliente: "+cliente); 			
+				ApplicationPagos.funcion_ajax_ventas_credito(cliente,null,null);		
 			}	
 	}else{
 			if(! (ApplicationPagos.fechas.isVisible())){
-				ApplicationPagos.funcion_ajax_ventas(null,de,al);
-			}else{	
-				POS.aviso("Imposible", "No se puede buscar ya que no hay ninguna opcion seleccionada"); 
+				ApplicationPagos.funcion_ajax_ventas_credito(null,de,al);
+			}else{
+				ApplicationPagos.funcion_ajax_ventas_credito(null,null,null);
 			}
 	}
 };
+//clickBuscar
+
+//click Deudores
 this.clickDeudores = function ()
 {
+	//ponemos el tipo a 1 (deudores) y llamamos la funcion funcion_ajax_ventas_credito para llenar la lista
 	ApplicationPagos.tipo=1;
-	ApplicationPagos.funcion_ajax_ventas(null,null,null);
+	clickBuscar();
 };
+//clickDeudores
 
+//clickPagados
 this.clickPagados = function ()
 {
+	//ponemos el tipo a 2 (pagados) y llamamos la funcion funcion_ajax_ventas_credito para llenar la lista
 	ApplicationPagos.tipo=2;
-	ApplicationPagos.funcion_ajax_ventas(null,null,null);
+	clickBuscar();
 };
+//clickPagados
 
+//clickTodos
 this.clickTodos = function ()
 {
+	//ponemos el tipo a 3 (todos) y llamamos la funcion funcion_ajax_ventas_credito para llenar la lista
 	ApplicationPagos.tipo=3;
-	ApplicationPagos.funcion_ajax_ventas(null,null,null);
+	clickBuscar();
 };
+//clickTodos
 
+//clickCliente
 this.clickCliente = function ()
 {
+	//muestra el boton de seleccionar cliente o lo esconde si esta visible
 	if(ApplicationPagos.btnClientes.isVisible()){
 			ApplicationPagos.btnClientes.setVisible(true);
 			ApplicationPagos.formulario.setHeight(ApplicationPagos.formulario.getHeight()+30);
@@ -601,9 +724,12 @@ this.clickCliente = function ()
 			ApplicationPagos.formulario.setHeight(ApplicationPagos.formulario.getHeight()-30);
 	}
 };
+//clickCliente
 
+//clickPeriodo
 this.clickPeriodo = function ()
 {
+	//muestra el botones de seleccionar periodo o lo esconde si esta visible
 	if(ApplicationPagos.fechas.isVisible()){
 			ApplicationPagos.fechas.setVisible(true);
 			ApplicationPagos.formulario.setHeight(ApplicationPagos.formulario.getHeight()+80);
@@ -612,6 +738,7 @@ this.clickPeriodo = function ()
 			ApplicationPagos.formulario.setHeight(ApplicationPagos.formulario.getHeight()-80);
 	}
 };
+//clickPeriodo
 
 //autoinstalar esta applicacion
 AppInstaller( new ApplicationPagos() );
