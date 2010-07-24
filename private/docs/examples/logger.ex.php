fbda314ce4b928e14789bf7dd60c551ab6e06d87
Alan Gonzalez <alanboy@alanboy.net>
2010-07-24 04:12:24 +0000

    Restaurando base de datos, y eliminando documentacion anterior

--- private/docs/examples/logger.ex.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('../../../server/mx.caffeina.logger/logger.php');
-
-$logger->log('Mensaje sin nivel de prioridad');
-$logger->log('Este es otro mensaje. Y es critico', PEAR_LOG_CRIT);
-$logger->setIdent("Módulo de ventas");
-$logger->log('Este es un mensaje desde un módulo especifico');
-$logger->log('Mensaje desde un módulo y con priodidad', PEAR_LOG_ERR);
--- private/docs/generate-doc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pwd;
-phpdoc -t docs/ -ti "Documentación de POS" -o HTML:frames:phpedit -s on -pp on -ed examples/ -f ../../server/mx.caffeina.logger/logger.php,../../server/db/DBConnection.php,../../server/dispatcher.php,../../www/proxy.php
