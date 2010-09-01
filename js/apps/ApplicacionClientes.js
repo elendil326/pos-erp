dd53e039f92488443f9210fb9ce2c7da97eb7c09
Alan Gonzalez <alanboy@alanboy.net>
2010-09-01 04:31:53 +0000

    Agregando nuevos recursos del sencha 9.0.3, al parecer se corrigieron varias cosas

--- js/apps/ApplicacionClientes.js
+++ js/apps/ApplicacionClientes.js
@@ -305,23 +305,8 @@ ApplicacionClientes.prototype.addClientDetailsPanel= function( recor ){
 		  disabledClass: '',
           flex: 1
         },
-        //items: [carousel]
-		items: [{
-			        xtype: 'carousel',
-			        items: [{
-			            html: '<p>Navigate the carousel on this page by swiping left/right.</p>',
-			            cls: 'card card1'
-			        },
-			        {
-			            html: '<p>Clicking on either side of the indicators below</p>',
-			            cls: 'card card2'
-			        },
-			        {
-			            html: 'Card #3',
-			            cls: 'card card3'
-			        }
-					]
-				}]
+        items: [carousel]
+		
     	});
 
 
