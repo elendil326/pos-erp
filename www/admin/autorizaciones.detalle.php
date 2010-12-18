f359959c668975970ccee5a8dd96a49099c86524
Alan Gonzalez <alanboy@alanboy.net>
2010-12-18 07:20:56 +0000

    terminado responder autorizaciones

--- www/admin/autorizaciones.detalle.php
+++ www/admin/autorizaciones.detalle.php
@@ -26,18 +26,33 @@ switch( $autorizacionDetalles->clave ){
 
     case "201": 
         //solicitud de autorizcion de gasto
-        echo $autorizacionDetalles->concepto;
-        echo $autorizacionDetalles->monto;
+        ?>
+            <h2>Solicitud de gasto</h2>
+            <table>
+                <tr><td>Concepto</td><td><?php echo $autorizacionDetalles->concepto; ?></td></tr>
+                <tr><td>Monto</td><td><?php echo $autorizacionDetalles->monto; ?></td></tr>
+                <tr><td></td><td><input type=button value="Autorizar" onClick="contestar(<?php echo $_REQUEST['id'] ?>, true)"><input onClick="contestar(<?php echo $_REQUEST['id'] ?>, false)" type=button value="Rechazar"></td></tr>
+            </table>
+        <?php
+        
+
     break;
 
     case "202": 
         //cambio de limite de credito
-        echo $autorizacionDetalles->id_cliente;
-        echo $autorizacionDetalles->cantidad;
+        ?>
+            <h2>Solicitud de limite de credito</h2>
+            <table>
+                <tr><td>Cliente</td><td><?php echo $autorizacionDetalles->id_cliente; ?></td></tr>
+                <tr><td>Cantidad</td><td><?php echo $autorizacionDetalles->cantidad; ?></td></tr>
+                <tr><td></td><td><input type=button value="Autorizar" onClick="contestar(<?php echo $_REQUEST['id'] ?>, true)"><input onClick="contestar(<?php echo $_REQUEST['id'] ?>, false)" type=button value="Rechazar"></td></tr>
+            </table>
+        <?php
     break;
 
     case "203": 
         //devoluciones
+
         echo $autorizacionDetalles->id_venta;
         echo $autorizacionDetalles->id_producto;
         echo $autorizacionDetalles->cantidad;
@@ -45,26 +60,46 @@ switch( $autorizacionDetalles->clave ){
 
     case "204": 
         //cambio de precio
-        echo $autorizacionDetalles->id_producto;
-        echo $autorizacionDetalles->precio;
+        ?>
+            <h2>Solicitud de cambio de precio a producto</h2>
+            <table>
+                <tr><td>Cliente</td><td><?php echo $autorizacionDetalles->id_producto; ?></td></tr>
+                <tr><td>Cantidad</td><td><?php echo $autorizacionDetalles->precio; ?></td></tr>
+                <tr><td></td><td><input type=button value="Autorizar" onClick="contestar(<?php echo $_REQUEST['id'] ?>, true)"><input onClick="contestar(<?php echo $_REQUEST['id'] ?>, false)" type=button value="Rechazar"></td></tr>
+            </table>
+        <?php
 
     break;
 
     case "205": 
         //merma
-        echo $autorizacionDetalles->id_compra;
-        echo $autorizacionDetalles->id_producto;
-        echo $autorizacionDetalles->cantidad;
+        ?>
+            <h2>Solicitud de merma</h2>
+            <table>
+                <tr><td>Cliente</td><td><?php echo $autorizacionDetalles->id_compra; ?></td></tr>
+                <tr><td>Cantidad</td><td><?php echo $autorizacionDetalles->id_producto; ?></td></tr>
+                <tr><td>Cantidad</td><td><?php echo $autorizacionDetalles->cantidad; ?></td></tr>
+                <tr><td></td><td><input type=button value="Autorizar" onClick="contestar(<?php echo $_REQUEST['id'] ?>, true)"><input onClick="contestar(<?php echo $_REQUEST['id'] ?>, false)" type=button value="Rechazar"></td></tr>
+            </table>
+        <?php
     break;
 
     case "209": 
         //solicitud de surtir
+        ?>
+            <h2>Solicitud de merma</h2>
+            <table>
+                <tr><td>Producto</td><td>Cantidad solicitada</td></tr><?php
+                <?php
+                foreach ($autorizacionDetalles->productos as $producto)
+                {
+                    ?><tr><td><?php echo $autorizacionDetalles->id_producto; ?></td><td><?php echo $autorizacionDetalles->cantidad; ?></td></tr><?php
+                }
+                ?>
+                <tr><td></td><td><input type=button value="Surtir sucursal" ></td></tr>
+            </table>
+        <?php
 
-        foreach ($autorizacionDetalles->productos as $producto)
-        {
-            echo $producto->id_producto;
-            echo $producto->cantidad;            
-        }
         
     break;
 
@@ -72,3 +107,37 @@ switch( $autorizacionDetalles->clave ){
     default: 
 }
 ?>
+
+
+
+
+<script>
+function contestar(id, response){
+
+        //hacer ajaxaso
+        jQuery.ajaxSettings.traditional = true;
+
+        $.ajax({
+	      url: "../proxy.php",
+	      data: { 
+                action : 208, 
+                id_autorizacion : id,
+                reply : response ? "1" : "2"
+
+           },
+	      cache: false,
+	      success: function(data){
+		        response = jQuery.parseJSON(data);
+
+                if(response.success == false){
+                    alert(response.reason);
+                    return;
+                }
+
+
+                alert("Autorizacion respondida con exito.");
+                window.location = "autorizaciones.php?action=pendientes";
+	      }
+	    });
+}
+</script>
