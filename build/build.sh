d4895b144521415fd6cfc3740d6d551f3f437cbf
Alan Gonzalez <alanboy@alanboy.net>
2010-12-28 05:43:20 +0000

    agregados scripts para hacer el build de la app

---
+++ build/build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo "Iniciando build"
+
+# quitando la carpeta de documentacion
+if [ -d docs ]
+then
+	rm -rf docs
+fi
+
+
+# entrar en el directorio de los javascripts
+# y fusionar los archivos que estan dentro de la misma carpeta
+function parsefiles(){
+	echo "escaneando $1"
+
+	#pegar todos los js en un solo archivo
+	cat $1/*.js > $1/out
+	rm $1/*.js	
+	
+	#minificar ese archivo
+	./jslint <$1/out > $1/out.js
+
+	rm $1/out
+
+
+	
+	if [ $(cat $1/out.js | wc -l) = "0" ]
+	then
+		#quitar archivos vacios
+		rm $1/out.js
+	else
+		# es un archivo con codigo, remover los if(DEBUG)
+		# if[ ]?[(]DEBUG[)][ {].*[ }]
+		
+		#touch $1/out
+		
+		#while read line
+		#do 
+		#	echo ${line//if[(]DEBUG[)][{].*[}]/} >> $1/out
+		#done < $1/out.js
+	fi
+
+	#buscar otros directorios para recursion
+	for i in $1/*
+	do
+		if [ -d $i ]
+		then
+			parsefiles $i
+		fi
+	done
+}
+
+
+parsefiles js
+
+
+#312.8 KB
+
