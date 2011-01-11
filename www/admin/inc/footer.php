4144f527b221108b309d6ec316027c60c82a9ea7
Alan Gonzalez <alanboy@alanboy.net>
2011-01-11 18:23:47 +0000

    Resoruces para nuevos estilos

---
+++ www/admin/inc/footer.php
@@ -0,0 +1,8 @@
+<!-- footer -->
+
+<p class="footer">
+   <a href="http://www.caffeina.mx">Página principal de Caffeina</a> - <a href="http://www.caffeina.mx/report/">Reportar una falla</a> - <a href="http://www.caffeina.mx/contacto.php">Envíenos un mensaje</a>
+    <br>
+    ©2010 Caffeina Mexico 
+<!-- <br><img src='../media/logo_simbolo.png'> -->
+</p>
