b131503fd1abcca1259e9d9694c8d7b7553b659e
Juan Manuel Garcia <zonademanel@gmail.com>
2011-05-03 08:09:46 +0000

    agregado los dao de compra_cliente y detalle_compra_cliente

---
+++ server/controller/detalle_compra_cliente.dao.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once ('Estructura.php');
+require_once("base/detalle_compra_cliente.dao.base.php");
+require_once("base/detalle_compra_cliente.vo.base.php");
+/** Page-level DocBlock .
+  * 
+  * @author no author especified
+  * @package docs
+  * 
+  */
+/** DetalleCompraCliente Data Access Object (DAO).
+  * 
+  * Esta clase contiene toda la manipulacion de bases de datos que se necesita para 
+  * almacenar de forma permanente y recuperar instancias de objetos {@link DetalleCompraCliente }. 
+  * @author no author especified
+  * @access public
+  * @package docs
+  * 
+  */
+class DetalleCompraClienteDAO extends DetalleCompraClienteDAOBase
+{
+
+}
