226dcbb5af712f5b39e5f3c029861d57b38f95e6
Alan Gonzalez <alanboy@alanboy.net>
2011-08-04 03:08:57 +0000

    información de debugging en login js para sucursales

--- js/login/start.js
+++ js/login/start.js
@@ -1,3 +1,4 @@
+/*jslint sloppy: true, eqeq: false, white: true, newcap: true, nomen: true, plusplus: true, maxerr: 50, indent: 4 */
 
 	/**
 	  *
@@ -18,44 +19,83 @@ var DEBUG = false,
 	  *
 	  *
 	  **/
-	POS = {U:{G:null}};
+	POS = {U:{G:null}},
+	
+	
+	form;
 
 
 
-if( document.location.search == "?debug" )
+if( document.location.search === "?debug" )
 {
-    DEBUG = true;
+	DEBUG = true;
 	console.log("Applicacion en modo de DEBUG !");
 }
 
-/**
-  * Buscar si el cliente existe
-  * 
-  *
-  **/
-function checkForClient()
-{
- 	Ext.getBody().mask('Iniciando...', 'x-mask-loading', false);
 
-	POS.ajaxToClient({
-		module : "handshake",
-		args : "",
-		success : function (){
-			//ok client is there...
-			checkForClientPIN();
-		},
-		failure: function (){
-			//client not found !
-			var html = "<div onClick='checkForClient()'>No se ha encontrado el cliente de caffeina."
-				+ "Toque aqui para reintentar."
-				+ "<div>";
 
-			Ext.getBody().mask( html, 'x-mask-loading', true );
-		},
-	});
+
+function checkCurrentSession( clientPIN ){
 	
-}
+	if(DEBUG){console.log("checkCurrentSession()");}
+	
+	Ext.getBody().mask('Validando sucursal...', 'x-mask-loading', false);
+	Ext.Ajax.request({
+		url: '../proxy.php',
+		params : {
+			action : 2001,
+			pin	   : clientPIN
+		},
+		
+		success: function(response, opts) {
 
+			if(response.responseText.length === 0){
+				Ext.getBody().mask( "Error." );
+				return;
+			}
+
+			Ext.getBody().unmask(); 
+			var results;
+			try{
+				results = Ext.util.JSON.decode( response.responseText );
+			}catch(e){
+				Ext.getBody().mask( "Error<br>Mostrando error orginal:<br>" + response.responseText );
+				return;				
+			}
+			//algo anda mal
+			if(!results.success){
+
+				if(DEBUG){
+					console.error("Basic testing has returned something other than success.");
+				}
+
+				Ext.getBody().mask( results.response );
+				return;
+			}
+			
+			
+			if( !results.sesion ){
+				//sesion no iniciada o no valida, mostrar la ventana de login
+				createLoginForm( results.sucursal ); 
+				return;
+			}
+			
+
+			window.location = "../proxy.php?action=2005";
+
+					   
+			
+		},
+
+		failure: function(response, opts) {
+			Ext.getBody().mask( "No se puede alcanzar el servidor. Verifique que tenga conectividad a internet." );
+			if(DEBUG){
+				console.log('server-side failure with status code ' + response.status);				
+			}
+
+		}
+	});
+}
 
 /**
   * Una vez que el se que el cliente existe, 
@@ -64,14 +104,17 @@ function checkForClient()
   **/
 function checkForClientPIN()
 {
- 	Ext.getBody().mask('Iniciando...', 'x-mask-loading', false);
-
+	Ext.getBody().mask('Iniciando...', 'x-mask-loading', false);
+	
+	if(DEBUG){console.log("Asking for PIN!");}
+	
 	POS.ajaxToClient({
 		module : "networking",
 		args : "",
 		success : function (a,b,c){
 
-
+			if(DEBUG){console.log("PIN retrival succesful!", a);}
+			
 			if(a.success === true){
 				checkCurrentSession(a.response);	
 				return;
@@ -88,7 +131,38 @@ function checkForClientPIN()
 				+ "<div>";
 
 			Ext.getBody().mask( html, 'x-mask-loading', true );
+		}
+	});
+	
+}
+
+/**
+  * Buscar si el cliente existe
+  * 
+  *
+  **/
+function checkForClient()
+{
+	Ext.getBody().mask('Iniciando...', 'x-mask-loading', false);
+
+	if(DEBUG){console.log("Enviando hanshake!");}
+
+	POS.ajaxToClient({
+		module : "handshake",
+		args : "",
+		success : function (){
+			//ok client is there...
+			if(DEBUG){console.log("Hanshake exitoso !");}
+			checkForClientPIN();
 		},
+		failure: function (){
+			//client not found !
+			var html = "<div onClick='checkForClient()'>No se ha encontrado el cliente de caffeina."
+				+ "Toque aqui para reintentar."
+				+ "<div>";
+
+			Ext.getBody().mask( html, 'x-mask-loading', true );
+		}
 	});
 	
 }
@@ -97,75 +171,19 @@ function checkForClientPIN()
 
 
 
-function checkCurrentSession( clientPIN ){
-	Ext.getBody().mask('Validando sucursal...', 'x-mask-loading', false);
-	Ext.Ajax.request({
-		url: '../proxy.php',
-		params : {
-			action : 2001,
-			pin    : clientPIN
-		},
-		
-		success: function(response, opts) {
-
-            if(response.responseText.length == 0){
-    			Ext.getBody().mask( "Error." );
-                return;
-            }
-
-			Ext.getBody().unmask();	
-			
-          	try{
-				var results = Ext.util.JSON.decode( response.responseText );
-			}catch(e){
-				Ext.getBody().mask( "Error<br>Mostrando error orginal:<br>" + response.responseText );
-  				return;				
-			}
-			//algo anda mal
-			if(!results.success){
-
-  				if(DEBUG){
-  					console.error("Basic testing has returned something other than success.");
-  				}
-
-				Ext.getBody().mask( results.response );
-  				return;
-			}
-			
-			
-			if( !results.sesion ){
-				//sesion no iniciada o no valida, mostrar la ventana de login
-				createLoginForm( results.sucursal ); 
-				return;
-			}
-			
-
-			window.location = "../proxy.php?action=2005";
-
-                       
-			
-		},
-
-		failure: function(response, opts) {
-			Ext.getBody().mask( "No se puede alcanzar el servidor. Verifique que tenga conectividad a internet." );
-			if(DEBUG){
-				console.log('server-side failure with status code ' + response.status);				
-			}
-
-		}
-	});
-}
 
 
 
-function parseLoginResults( args ){
+
+
+function parseLoginResults( args )
+{
 	
 	
 	if(!args.success){
 		//login invalido
-		
-        form.getComponent(0).setInstructions(args.text);
-        form.show();
+		form.getComponent(0).setInstructions(args.text);
+		form.show();
 		return;
 	}
 	
@@ -183,7 +201,7 @@ var sendLogin = function (){
 	
 	Ext.getBody().mask('Revisando...', 'x-mask-loading', true);
 	
- 	Ext.Ajax.request({
+	Ext.Ajax.request({
 		url: '../proxy.php',
 		params : {
 			action: 2004,
@@ -208,12 +226,12 @@ var sendLogin = function (){
 
 
 Ext.setup({
-    glossOnIcon: false,
-    onReady: function() {
-
+	glossOnIcon: false,
+	onReady: function() {
+		if(DEBUG){console.log("Im ready to start !");}
 		checkForClient();
 
-    }
+	}
 });
 
 
@@ -226,25 +244,25 @@ Ext.setup({
 function createLoginForm( sucursal ){
 
 	var formBase = {
-        scroll: null,
-        standardSubmit : false,
-        submitOnAction : false,
-        items: [
-            {
-                xtype: 'fieldset',
-                title: 'Bienvenido a ' + sucursal ,
-                instructions: 'Porfavor llene la informacion apropiada.',
-                defaults: {
-                    required: false,
-                    labelAlign: 'left'
-                },
-                items: [
-                {
-                    xtype: 'textfield',
-                    id : 'uid',
-                    label: 'Identifiaci&oacute;n',
-                    useClearIcon: true,
-                    autoCapitalize : false,
+		scroll: null,
+		standardSubmit : false,
+		submitOnAction : false,
+		items: [
+			{
+				xtype: 'fieldset',
+				title: 'Bienvenido a ' + sucursal ,
+				instructions: 'Porfavor llene la informacion apropiada.',
+				defaults: {
+					required: false,
+					labelAlign: 'left'
+				},
+				items: [
+				{
+					xtype: 'textfield',
+					id : 'uid',
+					label: 'Identifiaci&oacute;n',
+					useClearIcon: true,
+					autoCapitalize : false,
 					listeners : {
 						"focus" : function(){
 							kconf = {
@@ -257,11 +275,11 @@ function createLoginForm( sucursal ){
 							POS.Keyboard.Keyboard( this, kconf );
 						}
 					}
-                }, {
-                    xtype: 'passwordfield',
-                    id : 'pswd',
-                    label: 'Contrase&ntilde;a',
-                    useClearIcon: false,
+				}, {
+					xtype: 'passwordfield',
+					id : 'pswd',
+					label: 'Contrase&ntilde;a',
+					useClearIcon: false,
 					listeners : {
 						"focus" : function(){
 							kconf = {
@@ -274,59 +292,59 @@ function createLoginForm( sucursal ){
 							POS.Keyboard.Keyboard( this, kconf );
 						}
 					}	
-                }]
-            }
-        ],
-        listeners : {
-            submit : function(form, result){
-                if(DEBUG){console.log('success', Ext.toArray(result, form));}
+				}]
+			}
+		],
+		listeners : {
+			submit : function(form, result){
+				if(DEBUG){console.log('success', Ext.toArray(result, form));}
 				return;
-            },
-            exception : function(form, result){
-                //console.log('failure', Ext.toArray(arguments));
-            }
-        },
-    
-        dockedItems: [
-            {
-                xtype: 'toolbar',
-                dock: 'bottom',
-                items: [
-                    {xtype: 'spacer'},
-                    {
-                        text: 'Vaciar',
-                        handler: function() {
-                            form.getComponent(0).setInstructions( "Porfavor llene la informacion apropiada." );
-                            form.reset();
-                        }
-                    },
-                    {
-                        text: 'Entrar',
-                        ui: 'confirm',
-                        handler: sendLogin
-                    }
-                ]
-            }
-        ]
-    };
-    
-    
-    if (Ext.is.Phone) {
-        formBase.fullscreen = true;
-    } else {
-        Ext.apply(formBase, {
-            autoRender: true,
-            floating: true,
-            modal: true,
-            centered: true,
-            hideOnMaskTap: false,
-            height: 285,
-            width: 480
-        });
-    }
-    
-    form = new Ext.form.FormPanel(formBase);
-    form.show();
+			},
+			exception : function(form, result){
+				//console.log('failure', Ext.toArray(arguments));
+			}
+		},
+	
+		dockedItems: [
+			{
+				xtype: 'toolbar',
+				dock: 'bottom',
+				items: [
+					{xtype: 'spacer'},
+					{
+						text: 'Vaciar',
+						handler: function() {
+							form.getComponent(0).setInstructions( "Porfavor llene la informacion apropiada." );
+							form.reset();
+						}
+					},
+					{
+						text: 'Entrar',
+						ui: 'confirm',
+						handler: sendLogin
+					}
+				]
+			}
+		]
+	};
+	
+	
+	if (Ext.is.Phone) {
+		formBase.fullscreen = true;
+	} else {
+		Ext.apply(formBase, {
+			autoRender: true,
+			floating: true,
+			modal: true,
+			centered: true,
+			hideOnMaskTap: false,
+			height: 285,
+			width: 480
+		});
+	}
+	
+	form = new Ext.form.FormPanel(formBase);
+	form.show();
 }
 
 
