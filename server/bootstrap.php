096759e8671c9c163be3f71ce36daf1bc624fd01
Alan Gonzalez <alanboy@alanboy.net>
2011-09-19 22:38:04 +0000

    bugfix

--- server/bootstrap.php
+++ server/bootstrap.php
@@ -15,7 +15,7 @@
 	# *******************************
 	# Buscar la ruta de SERVER
 	# *******************************
-	define('POS_PATH_TO_SERVER_ROOT', dirname(__DIR__) . "/server"); 
+	define('POS_PATH_TO_SERVER_ROOT', dirname(__DIR__)  ); 
 	ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . POS_PATH_TO_SERVER_ROOT);
 
 
