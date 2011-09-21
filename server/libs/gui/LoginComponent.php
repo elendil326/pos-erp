46bc6de653bb5c419ffd0b51f7ef598693805047
Alan Gonzalez <alanboy@alanboy.net>
2011-09-21 13:18:19 +0000

    login was doing some unnecesary javascript

--- server/libs/gui/LoginComponent.php
+++ server/libs/gui/LoginComponent.php
@@ -23,13 +23,13 @@ class LoginComponent implements GuiComponent
 		return  '<div align="center" >'
 				. '<form action="' . $this->next_hop . '" method="POST">'
 				. '<table border="0" cellspacing="5" cellpadding="5">'
-				. '<tr><td rowspan="5"><img width="256" height="256" src="../media/safe.png">'
+				. '<tr><td rowspan="5"><img width="256" height="256" src="../../media/safe.png">'
 				. '</td></tr><tr><td></td><td colspan=2><h2>Inicio de sesion</h2></td></tr><tr>'
 				. '<td></td><td align="right">Usuario</td><td><input type="text" name="user" size="40"/>'
 				. '</td></tr><tr><td></td><td align="right">Contrase&ntilde;a</td><td>'
-				. '<input type="password" name="password" size="40" onkeypress="handleKeyPress(event)"/>'
+				. '<input type="password" name="password" size="40" onkeypress=""/>'
 				. '</td></tr><tr valign="top"><td></td><td></td><td align="right">'
-				. '<input type="submit" onclick="doStart()" value="Ingresar" onkeypress="handleKeyPress(event)"/>'
+				. '<input type="submit" value="Ingresar" onkeypress=""/>'
 				. '</td></tr></table><input type="hidden" name="jedi_login" value=1></form></div>';
 
 	}
