107cb5e9aafe7bdcfed7b8f95f66e881523cc1de
Andres Zendejas <panda326@gmail.com>
2011-10-20 13:08:41 +0000

    Agregado de unidad.dao.php

---
+++ server/model/unidad.dao.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once ('Estructura.php');
+require_once("base/unidad.dao.base.php");
+require_once("base/unidad.vo.base.php");
+/** Page-level DocBlock .
+  * 
+  * @author Andres
+  * @package docs
+  * 
+  */
+/** Unidad Data Access Object (DAO).
+  * 
+  * Esta clase contiene toda la manipulacion de bases de datos que se necesita para 
+  * almacenar de forma permanente y recuperar instancias de objetos {@link Unidad }. 
+  * @author Andres
+  * @access public
+  * @package docs
+  * 
+  */
+class UnidadDAO extends UnidadDAOBase
+{
+
+}
