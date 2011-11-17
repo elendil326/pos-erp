a106ca64908138f59617a3587dd5ac6dcbef159f
Andres Zendejas <panda326@gmail.com>
2011-11-17 14:53:11 +0000

    Metodo nuevo billete terminado con validacion

--- server/controllers/Efectivo.controller.php
+++ server/controllers/Efectivo.controller.php
@@ -8,6 +8,187 @@ require_once("interfaces/Efectivo.interface.php");
 	
   class EfectivoController implements IEfectivo{
   
+      
+        //Metodo para pruebas que simula la obtencion del id de la sucursal actual
+        private static function getSucursal()
+        {
+            return 1;
+        }
+        
+        //metodo para pruebas que simula la obtencion del id de la caja actual
+        private static function getCaja()
+        {
+            return 1;
+        }
+        
+        
+        /*
+         *Se valida que un string tenga longitud en un rango de un maximo inclusivo y un minimo exclusvio.
+         *Regresa true cuando es valido, y un string cuando no lo es.
+         */
+          private static function validarString($string, $max_length, $nombre_variable,$min_length=0)
+	{
+		if(strlen($string)<=$min_length||strlen($string)>$max_length)
+		{
+		    return "La longitud de la variable ".$nombre_variable." proporcionada (".$string.") no esta en el rango de ".$min_length." - ".$max_length;
+		}
+		return true;
+        }
+
+
+        /*
+         * Se valida que un numero este en un rango de un maximo y un minimo inclusivos
+         * Regresa true cuando es valido, y un string cuando no lo es
+         */
+	private static function validarNumero($num, $max_length, $nombre_variable, $min_length=0)
+	{
+	    if($num<$min_length||$num>$max_length)
+	    {
+	        return "La variable ".$nombre_variable." proporcionada (".$num.") no esta en el rango de ".$min_length." - ".$max_length;
+	    }
+	    return true;
+	}
+        
+        /*
+         * Valida los parametros de la tabla billete. Regresa un string con el error en caso de encontrarse alguno.
+         * De lo contrario, regresa verdadero
+         */
+        private static function validarParametrosBillete
+        (
+                $id_billete = null,
+                $id_moneda = null,
+                $nombre = null,
+                $valor = null,
+                $foto_billete = null,
+                $activo = null
+        )
+        {
+            //valida que el billete exista y que este activo
+            if(!is_null($id_billete))
+            {
+                $billete = BilleteDAO::getByPK($id_billete);
+                if(is_null($billete))
+                    return "El billete ".$id_billete." no existe";
+                
+                if(!$billete->getActivo())
+                    return "El billete ".$id_billete." no esta activo";
+            }
+            
+            //valida que la moneda exista y que sea activa
+            if(!is_null($id_moneda))
+            {
+                $e = self::validarParametrosMoneda($id_moneda);
+                if(is_string($e))
+                    return $e;
+            }
+            
+            //valida que el nombre este en rango y que no se repita
+            if(!is_null($nombre))
+            {
+                $e = self::validarString($nombre, 50, "nombre");
+                if(is_string($e))
+                    return $e;
+                
+                $billetes = BilleteDAO::search( new Billete( array( "nombre" => trim($nombre) ) ) );
+                foreach($billetes as $billete)
+                {
+                    if($billete->getActivo())
+                        return "El nombre (".$nombre.") ya esta siendo usado por el billete ".$billete->getIdBillete();
+                }
+            }
+            
+            //valida que el valor este en rango
+            if(!is_null($valor))
+            {
+                $e = self::validarNumero($valor, 1.8e200, "valor");
+                if(is_string($e))
+                    return $e;
+            }
+            
+            //valida que la foto del billete este en rango
+            if(!is_null($foto_billete))
+            {
+                $e = self::validarString($foto_billete, 100, "foto de billete");
+                if(is_string($e))
+                    return $e;
+            }
+            
+            //valida el boleano activo
+            if(!is_null($activo))
+            {
+                $e = self::validarNumero($activo, 1, "activo");
+                if(is_string($e))
+                    return $e;
+            }
+            
+            //no se encontro error
+            return true;
+        }
+        
+        /*
+         * Valida los parametros de la tabla moneda. Regresa un string con el error en caso de encontrarse alguno.
+         * De lo contrario, regresa verdadero
+         */
+        private static function validarParametrosMoneda
+        (
+                $id_moneda = null,
+                $nombre = null,
+                $simbolo = null,
+                $activa = null
+        )
+        {
+            //valida que la moneda exista y que este activa
+            if(!is_null($id_moneda))
+            {
+                $moneda = MonedaDAO::getByPK($id_moneda);
+                if(is_null($moneda))
+                    return "La moneda ".$id_moneda." no existe";
+                
+                if(!$moneda->getActiva())
+                    return "La moneda ".$id_moneda." no esta activa";
+            }
+            
+            //valida que el nombre sea valido y que no se repita
+            if(!is_null($nombre))
+            {
+                $e = self::validarString($nombre, 100, "nombre");
+                if(is_string($e))
+                    return $e;
+                
+                $monedas = MOnedaDAO::search( new Moneda( array( "nombre" => trim($nombre) ) ) );
+                foreach($monedas as $moneda)
+                {
+                    if($moneda->getActiva())
+                        return "El nombre (".$nombre.") ya esta en uso por la moneda ".$moneda->getIdMoneda();
+                }
+            }
+            
+            //valida que el simbolo este en rango
+            if(!is_null($simbolo))
+            {
+                $e = self::validarString($simbolo, 10, "simbolo");
+                if(is_string($e))
+                    return $e;
+            }
+            
+            //valida el boleano activa
+            if(!is_null($activa))
+            {
+                $e = self::validarNumero($activa, 1, "activa");
+                if(is_string($e))
+                    return $e;
+            }
+            
+            //no se encontro error
+            return true;
+        }
+      
+      
+      
+      
+      
+      
+      
   
 	/**
  	 *
@@ -24,11 +205,43 @@ require_once("interfaces/Efectivo.interface.php");
 		$nombre, 
 		$valor, 
 		$id_moneda, 
-		$foto_billete = ""
+		$foto_billete = null
 	)
 	{  
-  
-  
+            Logger::log("Creando nuevo billete");
+            
+            //se validan los parametros recibidos
+            $validar = self::validarParametrosBillete(null, $id_moneda, $nombre, $valor, $foto_billete);
+            if(is_string($validar))
+            {
+                Logger::error($validar);
+                throw new Exception($validar);
+            }
+            
+            //Se crea el nuevo billete y se guarda
+            $billete = new Billete(
+                    array(
+                        "nombre"        => $nombre,
+                        "valor"         => $valor,
+                        "id_moneda"     => $id_moneda,
+                        "foto_billete"  => $foto_billete,
+                        "activo"        => 1
+                    )
+                    );
+            DAO::transBegin();
+            try
+            {
+                BilleteDAO::save($billete);
+            }
+            catch(Exception $e)
+            {
+                DAO::transRollback();
+                Logger::error("No se ha podido crear el nuevo billete: ".$e);
+                throw new Exception("No se ha podido crear el nuevo billete");
+            }
+            DAO::transEnd();
+            Logger::log("Billete creado exitosamente");
+            return array( "id_billete" => $billete->getIdBillete() );
 	}
   
 	/**
