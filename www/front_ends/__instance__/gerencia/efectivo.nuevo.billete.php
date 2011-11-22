346e8178523ff3eb5d1b5e8f9c92ff0d85e71941
Andres Zendejas <panda326@gmail.com>
2011-11-22 16:04:46 +0000

    Formulario nuevo Billete terminado, esperando solucion de bug para pruebas

--- www/front_ends/__instance__/gerencia/efectivo.nuevo.billete.php
+++ www/front_ends/__instance__/gerencia/efectivo.nuevo.billete.php
@@ -7,5 +7,33 @@
 		require_once("../../../../server/bootstrap.php");
 
 		$page = new GerenciaComponentPage();
+                
+                //titulos
+	$page->addComponent( new TitleComponent( "Nuevo billete" ) );
 
+	//forma de nuevo billete
+	$form = new DAOFormComponent( array( new Billete() ) );
+	
+	$form->hideField( array( 
+			"id_billete",
+                        "activo"
+		 ));
+
+
+	$form->renameField( array( 
+			"nombre" 			=> "nombre"
+		));
+	
+	$form->addApiCall( "api/efectivo/billete/nuevo/" );
+	
+	$form->makeObligatory(array( 
+			"nombre",
+			"valor",
+			"id_moneda"
+		));
+	
+	$page->addComponent( $form );
+
+
+	//render the page
 		$page->render();
