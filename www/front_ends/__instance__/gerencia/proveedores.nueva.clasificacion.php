0746d029c66761be1804b5961817bab78970eca8
Andres Zendejas <panda326@gmail.com>
2011-11-24 16:28:05 +0000

    Formulario nueva clasificacion de proveedor terminado

--- www/front_ends/__instance__/gerencia/proveedores.nueva.clasificacion.php
+++ www/front_ends/__instance__/gerencia/proveedores.nueva.clasificacion.php
@@ -8,4 +8,34 @@
 
 		$page = new GerenciaComponentPage();
 
+                //titulos
+	$page->addComponent( new TitleComponent( "Nueva clasificacion de proveedor" ) );
+
+	//forma de nueva clasificacion de proveedor
+	$form = new DAOFormComponent( array( new ClasificacionProveedor() ) );
+	
+	$form->hideField( array( 
+			"id_clasificacion_proveedor",
+                        "activa"
+		 ));
+
+
+//	$form->renameField( array( 
+//			"nombre" 			=> "razon_social",
+//			"codigo_usuario"	=> "codigo_cliente"
+//		));
+	
+	$form->addApiCall( "api/proveedor/clasificacion/nueva/", "GET" );
+	
+	$form->makeObligatory(array( 
+			"nombre"
+		));
+	
+//	$form->createComboBoxJoin( "id_ciudad", "nombre", CiudadDAO::getAll( ) );
+//	$form->createComboBoxJoin( "id_clasificacion_cliente", "nombre", ClasificacionClienteDAO::getAll( ) );
+	
+	$page->addComponent( $form );
+
+
+	//render the page
 		$page->render();
