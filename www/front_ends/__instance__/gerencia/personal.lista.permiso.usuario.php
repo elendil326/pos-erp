6232aeeb5e90e310120b1055173a08399ee0506a
Andres Zendejas <panda326@gmail.com>
2011-11-24 23:27:33 +0000

    lista permisos de usuario terminado

--- www/front_ends/__instance__/gerencia/personal.lista.permiso.usuario.php
+++ www/front_ends/__instance__/gerencia/personal.lista.permiso.usuario.php
@@ -8,4 +8,33 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Permisos de Usuario" ) );
+		$page->addComponent( new MessageComponent( "Lista de usuarios con sus permisos" ) );
+		
+		$tabla = new TableComponent( 
+			array(
+				"id_permiso"    => "Permiso",
+                                "id_usuario"        => "Usuario"
+			),
+                         PersonalYAgentesController::ListaPermisoUsuario()
+		);
+                
+                function funcion_permiso($id_permiso)
+                {
+                    return PermisoDAO::getByPK($id_permiso) ? PermisoDAO::getByPK($id_permiso)->getPermiso() : "-----";
+                }
+                
+                function funcion_usuario($id_usuario)
+                {
+                    return UsuarioDAO::getByPK($id_usuario) ? UsuarioDAO::getByPK($id_usuario)->getNombre() : "-----";
+                }
+                
+                $tabla->addColRender("id_permiso", "funcion_permiso");
+                
+                $tabla->addColRender("id_usuario", "funcion_usuario");
+		
+		$tabla->addOnClick( "id_usuario", "(function(a){window.location = 'personal.usuario.ver.php?pid=' + a;})", false, true );
+		
+		$page->addComponent( $tabla );
+                
 		$page->render();
