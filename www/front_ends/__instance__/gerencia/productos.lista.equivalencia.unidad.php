8aa06326854effc77b99d65cafbeed19b47ad331
Andres Zendejas <panda326@gmail.com>
2011-11-25 23:21:51 +0000

    pagina para lista de equivalencia entre unidades terminada

--- www/front_ends/__instance__/gerencia/productos.lista.equivalencia.unidad.php
+++ www/front_ends/__instance__/gerencia/productos.lista.equivalencia.unidad.php
@@ -8,4 +8,29 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Equivalencias entre unidades" ) );
+		$page->addComponent( new MessageComponent( "Lista de equivalencias entre unidades" ) );
+		
+		$tabla = new TableComponent( 
+			array(
+				"id_unidad" => "Una unidad de",
+                                "equivalencia" => "Equivale a ",
+                                "id_unidades" => "Unidades"
+			),
+			ProductosController::Lista_equivalenciaUnidad()
+		);
+		
+                function funcion_unidad( $unidad )
+                {
+                    return UnidadDAO::getByPK($unidad) ? UnidadDAO::getByPK($unidad)->getNombre() : "????";
+                }
+                
+                $tabla->addColRender("id_unidad", "funcion_unidad");
+                $tabla->addColRender("id_unidades", "funcion_unidad");
+                
+		$tabla->addOnClick( "id_unidad", "(function(a){ window.location = 'unidad.ver.php?uid=' + a; })" );
+		
+			
+		$page->addComponent( $tabla );
+                
 		$page->render();
