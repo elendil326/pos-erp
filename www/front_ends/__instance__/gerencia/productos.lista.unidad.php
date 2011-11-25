819d6269dbe3edec44929686618a34a15400b6de
Andres Zendejas <panda326@gmail.com>
2011-11-25 23:15:00 +0000

    pagina de lista unidades terminada

--- www/front_ends/__instance__/gerencia/productos.lista.unidad.php
+++ www/front_ends/__instance__/gerencia/productos.lista.unidad.php
@@ -8,4 +8,35 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Unidades" ) );
+		$page->addComponent( new MessageComponent( "Lista unidades" ) );
+		
+		$tabla = new TableComponent( 
+			array(
+				"nombre" => "Nombre",
+                                "descripcion" => "Descripcion",
+                                "es_entero" => "Tipo de manejo",
+                                "activa" => "Activa"
+			),
+			ProductosController::ListaUnidad()
+		);
+		
+                function funcion_activa( $activa )
+                {
+                    return $activa ? "Activa" : "Inactiva";
+                }
+                
+                function funcion_es_entero( $es_entero )
+                {
+                    return $es_entero ? "Entero" : "Decimal";
+                }
+                
+                $tabla->addColRender("activa", "funcion_activa");
+                $tabla->addColRender("es_entero", "funcion_es_entero");
+                
+		$tabla->addOnClick( "id_unidad", "(function(a){ window.location = 'unidad.ver.php?uid=' + a; })" );
+		
+			
+		$page->addComponent( $tabla );
+                
 		$page->render();
