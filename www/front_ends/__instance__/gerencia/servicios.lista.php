bc1f2e6bf2d11c71d570fd384ae8873d962d3ea2
Andres Zendejas <panda326@gmail.com>
2011-11-25 23:32:28 +0000

    pagina de lista de servicios terminada

--- www/front_ends/__instance__/gerencia/servicios.lista.php
+++ www/front_ends/__instance__/gerencia/servicios.lista.php
@@ -8,4 +8,31 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Servicios" ) );
+		$page->addComponent( new MessageComponent( "Lista de servicios" ) );
+		
+		$tabla = new TableComponent( 
+			array(
+                                "codigo_servicio" => "Codigo de servicio",
+				"nombre_servicio" => "Nombre",
+                                "metodo_costeo" => "Metodo de costeo",
+                                "precio" => "Precio",
+                                "margen_de_utilidad" => "Margen de Utilidad",
+                                "activo" => "Activo"
+			),
+			ServiciosController::Lista()
+		);
+		
+                function funcion_activo( $activo )
+                {
+                    return ($activo) ? "Activo" : "Inactivo";
+                }
+                
+                $tabla->addColRender("activo", "funcion_activo");
+                
+		$tabla->addOnClick( "id_servicio", "(function(a){ window.location = 'servicio.ver.php?sid=' + a; })" );
+		
+			
+		$page->addComponent( $tabla );
+                
 		$page->render();
