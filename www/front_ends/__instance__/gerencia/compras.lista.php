1d07f0e7f469349640f93acf1724c7d0e0cfaded
Andres Zendejas <panda326@gmail.com>
2011-11-28 16:45:27 +0000

    pagina lista de compras terminada

--- www/front_ends/__instance__/gerencia/compras.lista.php
+++ www/front_ends/__instance__/gerencia/compras.lista.php
@@ -8,4 +8,40 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Compras" ) );
+		$page->addComponent( new MessageComponent( "Lista de compras" ) );
+		
+		$tabla = new TableComponent( 
+			array(
+                                "id_vendedor_compra"     => "Proveedor",
+				"tipo_de_compra"         => "Tipo de compra",
+				"subtotal"               => "Subtotal",
+                                "impuesto"               => "Impuesto",
+                                "descuento"              => "Descuento",
+                                "retencion"              => "Retencion",
+                                "total"                  => "Total",
+                                "saldo"                  => "Saldo",
+                                "cancelada"              => "Cancelada",
+                                "fecha"                  => "Fecha"
+			),
+                         ComprasController::Lista()
+		);
+//                
+                function funcion_vendedor($id_vendedor)
+                {
+                    return (UsuarioDAO::getByPK($id_vendedor) ? UsuarioDAO::getByPK($id_vendedor)->getNombre() : "-----" );
+                }
+                
+                function funcion_cancelada($cancelada)
+                {
+                    return (($cancelada) ? "Cancelada" : "Activa" );
+                }
+                
+                $tabla->addColRender("id_vendedor_compra", "funcion_vendedor");
+                $tabla->addColRender("cancelada", "funcion_cancelada");
+                
+		$tabla->addOnClick( "id_compra", "(function(a){ window.location = 'compra.ver.php?cid=' + a; })" );
+                
+                $page->addComponent( $tabla );
+                
 		$page->render();
