94da5327751ba1dba2ded6fc7bbe085a39c1548d
Andres Zendejas <panda326@gmail.com>
2011-11-28 14:24:41 +0000

    pagina lista de paquetes terminada

--- www/front_ends/__instance__/gerencia/paquetes.lista.php
+++ www/front_ends/__instance__/gerencia/paquetes.lista.php
@@ -8,4 +8,31 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Paquetes" ) );
+		$page->addComponent( new MessageComponent( "Lista de paquetes " ) );
+		
+		$tabla = new TableComponent( 
+			array(
+                                "nombre" => "Nombre",
+                                "margen_utilidad" => "Margen_utilidad",
+                                "precio"    => "Precio",
+                                "descuento" => "Descuento",
+                                "foto_paquete" => "Foto de paquete",
+                                "activo" => "Activo"
+			),
+			PaquetesController::Lista()
+		);
+                
+                function funcion_activo( $activo )
+                {
+                    return ($activo) ? "Activo" : "Inactivo";
+                }
+                
+                $tabla->addColRender("activo", "funcion_activo");
+                
+		$tabla->addOnClick( "id_paquete", "(function(a){ window.location = 'paquete.ver.php?pid=' + a; })" );
+		
+			
+		$page->addComponent( $tabla );
+                
 		$page->render();
