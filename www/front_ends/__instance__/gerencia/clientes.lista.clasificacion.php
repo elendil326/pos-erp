2635e05874b8672ca901dff2c876786270ab660f
Andres Zendejas <panda326@gmail.com>
2011-11-28 14:32:38 +0000

    pagina de lista de clasificacion de clientes terminada

--- www/front_ends/__instance__/gerencia/clientes.lista.clasificacion.php
+++ www/front_ends/__instance__/gerencia/clientes.lista.clasificacion.php
@@ -8,4 +8,30 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Clasificaciones de cliente" ) );
+		$page->addComponent( new MessageComponent( "Lista de clasificaciones de cliente " ) );
+		
+		$tabla = new TableComponent( 
+			array(
+                                "clave_interna" => "Clave interna",
+                                "nombre" => "Nombre",
+                                "descripcion" => "Descripcion",
+                                "margen_utilidad"    => "Margen de utilidad",
+                                "descuento" => "Descuento"
+			),
+			ClientesController::ListaClasificacion()
+		);
+                
+//                function funcion_activo( $activo )
+//                {
+//                    return ($activo) ? "Activo" : "Inactivo";
+//                }
+//                
+//                $tabla->addColRender("activo", "funcion_activo");
+                
+		$tabla->addOnClick( "id_clasificacion_cliente", "(function(a){ window.location = 'cliente.clasificacion.ver.php?cid=' + a; })" );
+		
+			
+		$page->addComponent( $tabla );
+                
 		$page->render();
