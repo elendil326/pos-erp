7c2b7f282bd329c879c521a2d97de4100d3a8799
Andres Zendejas <panda326@gmail.com>
2011-11-28 14:40:24 +0000

    pagina lista de clasificaciones de proveedr terminada

--- www/front_ends/__instance__/gerencia/proveedores.lista.clasificacion.php
+++ www/front_ends/__instance__/gerencia/proveedores.lista.clasificacion.php
@@ -7,5 +7,29 @@
 		require_once("../../../../server/bootstrap.php");
 
 		$page = new GerenciaComponentPage();
-
+                
+                $page->addComponent( new TitleComponent( "Clasificaciones de proveedor" ) );
+		$page->addComponent( new MessageComponent( "Lista de clasificaciones de proveedor" ) );
+		
+		$tabla = new TableComponent( 
+			array(
+                                "nombre" => "Nombre",
+                                "descripcion" => "Descripcion",
+                                "activa" => "Activa"
+			),
+			ProveedoresController::ListaClasificacion()
+		);
+                
+                function funcion_activa( $activa )
+                {
+                    return ($activa) ? "Activa" : "Inactiva";
+                }
+                
+                $tabla->addColRender("activa", "funcion_activa");
+                
+		$tabla->addOnClick( "id_clasificacion_proveedor", "(function(a){ window.location = 'proveedor.clasificacion.ver.php?pid=' + a; })" );
+		
+			
+		$page->addComponent( $tabla );
+                
 		$page->render();
