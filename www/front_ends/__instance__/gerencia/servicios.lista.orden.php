3270def9b9fcfddbc7a3a42ea3028b88cfba0576
Andres Zendejas <panda326@gmail.com>
2011-11-28 14:24:28 +0000

    pagina lista de ordenes de servicio terminada

--- www/front_ends/__instance__/gerencia/servicios.lista.orden.php
+++ www/front_ends/__instance__/gerencia/servicios.lista.orden.php
@@ -8,4 +8,54 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Ordenes de servicio" ) );
+		$page->addComponent( new MessageComponent( "Lista de ordenes de servicio" ) );
+		
+		$tabla = new TableComponent( 
+			array(
+                                "id_servicio" => "Servicio",
+				"id_usuario_venta" => "Cliente",
+                                "fecha_orden" => "Fecha Orden",
+                                "fecha_entrega" => "Fecha Entrega",
+                                "adelanto" => "Adelanto",
+                                "activa" => "Activa",
+                                "cancelada" => "Cancelada",
+                                "descripcion" => "Descripcion"
+			),
+			ServiciosController::ListaOrden()
+		);
+		
+                function funcion_servicio( $servicio )
+                {
+                    return ServicioDAO::getByPK($servicio) ? ServicioDAO::getByPK($servicio)->getNombreServicio() : "????";
+                }
+                
+                function funcion_usuario_venta( $usuario_venta )
+                {
+                    return UsuarioDAO::getByPK($usuario_venta) ? UsuarioDAO::getByPK($usuario_venta)->getNombre() : "????";
+                }
+                
+                function funcion_activa( $activa )
+                {
+                    return ($activa) ? "Activa" : "Inactiva";
+                }
+                
+                function funcion_cancelada( $cancelada )
+                {
+                    return ($cancelada) ? "Cancelada" : "No Cancelada";
+                }
+                
+                $tabla->addColRender("activa", "funcion_activa");
+                
+                $tabla->addColRender("cancelada", "funcion_cancelada");
+                
+                $tabla->addColRender("id_servicio", "funcion_servicio");
+                
+                $tabla->addColRender("id_usuario_venta", "funcion_usuario_venta");
+                
+		$tabla->addOnClick( "id_servicio", "(function(a){ window.location = 'servicio.orden.ver.php?oid=' + a; })" );
+		
+			
+		$page->addComponent( $tabla );
+                
 		$page->render();
