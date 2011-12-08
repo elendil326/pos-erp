92fe9af496119ad16a8cdb264be4e71e480bd638
Andres Zendejas <panda326@gmail.com>
2011-12-08 23:09:57 +0000

    formulario de editar rol

--- www/front_ends/__instance__/gerencia/personal.editar.rol.php
+++ www/front_ends/__instance__/gerencia/personal.editar.rol.php
@@ -7,5 +7,29 @@
 		require_once("../../../../server/bootstrap.php");
 
 		$page = new GerenciaComponentPage();
+                
+                //
+		// Parametros necesarios
+		// 
+		$page->requireParam(  "rid", "GET", "Este rol no existe." );
+		$este_rol = RolDAO::getByPK( $_GET["rid"] );
+		
+		//
+		// Titulo de la pagina
+		// 
+		$page->addComponent( new TitleComponent( "Editar rol de " . $este_rol->getNombre() , 2 ));
 
+		//
+		// Forma de rol
+		// 
+		$form = new DAOFormComponent( $este_rol );
+		$form->hideField( array( 
+				"id_rol",
+			 ));
+                
+                $form->addApiCall( "api/personal/rol/editar/" );
+                
+	    //$form->createComboBoxJoin("id_unidad", "nombre", UnidadDAO::search( new Unidad( array( "activa" => 1 ) ) ));
+		$page->addComponent( $form );
+                
 		$page->render();
