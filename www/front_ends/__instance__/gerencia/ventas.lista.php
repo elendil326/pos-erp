3924ea7bd954418a36b96a025a134e6593ae3d4c
Andres Zendejas <panda326@gmail.com>
2011-12-08 23:07:59 +0000

    Pagina de lista de ventas

--- www/front_ends/__instance__/gerencia/ventas.lista.php
+++ www/front_ends/__instance__/gerencia/ventas.lista.php
@@ -8,4 +8,40 @@
 
 		$page = new GerenciaComponentPage();
 
+                $page->addComponent( new TitleComponent( "Ventas" ) );
+		$page->addComponent( new MessageComponent( "Lista de ventas" ) );
+		
+		$tabla = new TableComponent( 
+			array(
+                                "id_comprador_venta"     => "Cliente",
+				"tipo_de_venta"         => "Tipo de venta",
+				"subtotal"               => "Subtotal",
+                                "impuesto"               => "Impuesto",
+                                "descuento"              => "Descuento",
+                                "retencion"              => "Retencion",
+                                "total"                  => "Total",
+                                "saldo"                  => "Saldo",
+                                "cancelada"              => "Cancelada",
+                                "fecha"                  => "Fecha"
+			),
+                         VentasController::Lista()
+		);
+//                
+                function funcion_comprador($id_comprador)
+                {
+                    return (UsuarioDAO::getByPK($id_comprador) ? UsuarioDAO::getByPK($id_comprador)->getNombre() : "-----" );
+                }
+                
+                function funcion_cancelada($cancelada)
+                {
+                    return (($cancelada) ? "Cancelada" : "Activa" );
+                }
+                
+                $tabla->addColRender("id_comprador_venta", "funcion_comprador");
+                $tabla->addColRender("cancelada", "funcion_cancelada");
+                
+		$tabla->addOnClick( "id_venta", "(function(a){ window.location = 'venta.ver.php?vid=' + a; })" );
+                
+                $page->addComponent( $tabla );
+                
 		$page->render();
