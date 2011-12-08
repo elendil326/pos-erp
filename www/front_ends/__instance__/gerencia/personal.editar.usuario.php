8c4f0c6f6415efabe9f07579f4e49149b5dc1934
Andres Zendejas <panda326@gmail.com>
2011-12-08 23:39:17 +0000

    Formulario de editar usuario

--- www/front_ends/__instance__/gerencia/personal.editar.usuario.php
+++ www/front_ends/__instance__/gerencia/personal.editar.usuario.php
@@ -8,4 +8,37 @@
 
 		$page = new GerenciaComponentPage();
 
+                //
+		// Parametros necesarios
+		// 
+		$page->requireParam(  "uid", "GET", "Este usuario no existe." );
+		$este_usuario = UsuarioDAO::getByPK( $_GET["uid"] );
+		
+		//
+		// Titulo de la pagina
+		// 
+		$page->addComponent( new TitleComponent( "Editar usuario " . $este_usuario->getNombre() , 2 ));
+
+		//
+		// Forma de usuario
+		// 
+		$form = new DAOFormComponent( $este_usuario );
+		$form->hideField( array( 
+				"id_usuario",
+                                "id_direccion",
+                                "id_direccion_alterna"
+			 ));
+                
+                $form->addApiCall( "api/personal/rol/editar/" );
+                
+                $form->createComboBoxJoin( "id_rol", "nombre", RolDAO::getAll(), $este_usuario->getIdRol() );
+
+                $form->createComboBoxJoin( "id_moneda", "nombre", MonedaDAO::search( new Moneda( array( "activa" => 1 ) ), $este_usuario->getIdMoneda() ) );
+
+                $form->createComboBoxJoin( "id_clasificacion_cliente", "nombre", ClasificacionClienteDAO::getAll(), $este_usuario->getIdClasificacionCliente() );
+
+                $form->createComboBoxJoin( "id_clasificacion_proveedor", "nombre", ClasificacionProveedorDAO::search( new ClasificacionProveedor( array( "activa" => 1 ) ) ), $este_usuario->getIdClasificacionProveedor() );
+            
+		$page->addComponent( $form );
+                
 		$page->render();
