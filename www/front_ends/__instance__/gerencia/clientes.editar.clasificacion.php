c49e6c70847197b29dd60758798a726698c55b6e
Andres Zendejas <panda326@gmail.com>
2011-12-09 23:22:04 +0000

    formulario para editar clasificacion de cliente terminado

--- www/front_ends/__instance__/gerencia/clientes.editar.clasificacion.php
+++ www/front_ends/__instance__/gerencia/clientes.editar.clasificacion.php
@@ -8,4 +8,39 @@
 
 		$page = new GerenciaComponentPage();
 
+                //
+		// Parametros necesarios
+		// 
+		$page->requireParam(  "cid", "GET", "Esta clasificacion de cliente no existe." );
+		$esta_clasificacion = ClasificacionClienteDAO::getByPK( $_GET["cid"] );
+                //titulos
+	$page->addComponent( new TitleComponent( "Editar clasificacion de cliente: ".$esta_clasificacion->getNombre() ) );
+
+	//forma de nuevo paquete
+	$form = new DAOFormComponent( $esta_clasificacion );
+	
+	$form->hideField( array( 
+			"id_clasificacion_cliente"
+		 ));
+
+
+//	$form->renameField( array( 
+//			"nombre" 			=> "razon_social",
+//			"codigo_usuario"	=> "codigo_cliente"
+//		));
+	
+	$form->addApiCall( "api/cliente/clasificacion/editar/", "GET" );
+	
+//	$form->makeObligatory(array( 
+//			"nombre"
+//		));
+	
+//	$form->createComboBoxJoin( "id_ciudad", "nombre", CiudadDAO::getAll( ) );
+//	$form->createComboBoxJoin( "id_clasificacion_cliente", "nombre", ClasificacionClienteDAO::getAll( ) );
+	
+	$page->addComponent( $form );
+
+
+	//render the page
+                
 		$page->render();
