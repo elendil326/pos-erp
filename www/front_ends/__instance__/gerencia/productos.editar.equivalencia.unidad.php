66ff90e0599bf420b97b6fe7ee6a4c816be9ccb9
Andres Zendejas <panda326@gmail.com>
2011-12-09 22:41:30 +0000

    formulario para editar equivalencias entre unidades

--- www/front_ends/__instance__/gerencia/productos.editar.equivalencia.unidad.php
+++ www/front_ends/__instance__/gerencia/productos.editar.equivalencia.unidad.php
@@ -8,4 +8,40 @@
 
 		$page = new GerenciaComponentPage();
 
+                 //titulos
+	$page->addComponent( new TitleComponent( "Editar equivalencia entre unidades" ) );
+
+	//forma de nueva equivalencia entre unidades
+	$form = new DAOFormComponent( array( new UnidadEquivalencia() ) );
+	
+//	$form->hideField( array( 
+//			"id_clasificacion_producto",
+//                        "activa"
+//		 ));
+
+//
+//	$form->renameField( array( 
+//			"nombre" 			=> "razon_social",
+//			"codigo_usuario"	=> "codigo_cliente"
+//		));
+	
+	$form->addApiCall( "api/producto/unidad/nueva_equivalencia/" , "GET");
+	
+	$form->makeObligatory(array( 
+			"id_unidad",
+                        "id_unidades",
+                        "equivalencia"
+		));
+	
+	$form->createComboBoxJoin( "id_unidad", "nombre", UnidadDAO::search( new Unidad( array( "activa" => 1 ) ) ) );
+        
+//        $form->renameField( array( "id_unidades" => "id_unidad" ) );
+//        
+        $form->createComboBoxJoinDistintName( "id_unidades","id_unidad", "nombre", UnidadDAO::search( new Unidad( array( "activa" => 1 ) ) ) ) ;
+//        
+//        $form->renameField( array( "id_unidad" => "id_unidades" ) );
+	
+	$page->addComponent( $form );
+        
+        //render the page
 		$page->render();
