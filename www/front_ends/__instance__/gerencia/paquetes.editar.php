f5b6d76bf97fb6fe08ddfb4fb6547afcd8e5db49
Andres Zendejas <panda326@gmail.com>
2011-12-09 23:17:46 +0000

    formulario para editar paquete terminado

--- www/front_ends/__instance__/gerencia/paquetes.editar.php
+++ www/front_ends/__instance__/gerencia/paquetes.editar.php
@@ -8,4 +8,39 @@
 
 		$page = new GerenciaComponentPage();
 
+                 //
+		// Parametros necesarios
+		// 
+		$page->requireParam(  "pid", "GET", "Este paquete no existe." );
+		$este_paquete = PaqueteDAO::getByPK( $_GET["pid"] );
+                //titulos
+	$page->addComponent( new TitleComponent( "Editar paquete" ) );
+
+	//forma de nuevo paquete
+	$form = new DAOFormComponent( $este_paquete );
+	
+	$form->hideField( array( 
+			"id_paquete",
+                        "activo"
+		 ));
+
+
+//	$form->renameField( array( 
+//			"nombre" 			=> "razon_social",
+//			"codigo_usuario"	=> "codigo_cliente"
+//		));
+	
+	$form->addApiCall( "api/paquete/editar/", "GET" );
+	
+//	$form->makeObligatory(array( 
+//			"nombre"
+//		));
+	
+//	$form->createComboBoxJoin( "id_ciudad", "nombre", CiudadDAO::getAll( ) );
+//	$form->createComboBoxJoin( "id_clasificacion_cliente", "nombre", ClasificacionClienteDAO::getAll( ) );
+	
+	$page->addComponent( $form );
+
+
+	//render the page
 		$page->render();
