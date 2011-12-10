54aa52d3cfb4a390401d578b40b3954ec6c3e956
Andres Zendejas <panda326@gmail.com>
2011-12-10 00:00:52 +0000

    formulario para editar clasificacion de proveedor terminado

--- www/front_ends/__instance__/gerencia/proveedores.editar.clasificacion.php
+++ www/front_ends/__instance__/gerencia/proveedores.editar.clasificacion.php
@@ -8,4 +8,38 @@
 
 		$page = new GerenciaComponentPage();
 
+                //
+		// Parametros necesarios
+		// 
+		$page->requireParam(  "cid", "GET", "Esta clasificacion de proveedor no existe." );
+		$esta_clasificacion = ClasificacionProveedorDAO::getByPK( $_GET["cid"] );
+                //titulos
+	$page->addComponent( new TitleComponent( "Editar clasificacion de proveedor: ".$esta_clasificacion->getNombre() ) );
+
+	//forma de nuevo paquete
+	$form = new DAOFormComponent( $esta_clasificacion );
+	
+	$form->hideField( array( 
+			"id_clasificacion_proveedor"
+		 ));
+
+
+//	$form->renameField( array( 
+//			"nombre" 			=> "razon_social",
+//			"codigo_usuario"	=> "codigo_cliente"
+//		));
+	
+	$form->addApiCall( "api/proveedor/clasificacion/editar/", "GET" );
+	
+//	$form->makeObligatory(array( 
+//			"nombre"
+//		));
+	
+//	$form->createComboBoxJoin( "id_ciudad", "nombre", CiudadDAO::getAll( ) );
+//	$form->createComboBoxJoin( "id_clasificacion_cliente", "nombre", ClasificacionClienteDAO::getAll( ) );
+	
+	$page->addComponent( $form );
+
+
+	//render the page
 		$page->render();
