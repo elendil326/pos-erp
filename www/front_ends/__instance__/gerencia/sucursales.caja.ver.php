564bf0c21e92e5359de827c3f47539a1c1da296f
Andres Zendejas <panda326@gmail.com>
2011-12-10 22:32:01 +0000

    Formulario de detalle de cajas con editar y eliminar terminado

---
+++ www/front_ends/__instance__/gerencia/sucursales.caja.ver.php
@@ -0,0 +1,77 @@
+<?php 
+
+
+
+		define("BYPASS_INSTANCE_CHECK", false);
+
+		require_once("../../../../server/bootstrap.php");
+
+		$page = new GerenciaComponentPage(  );
+
+
+		//
+		// Parametros necesarios
+		// 
+		$page->requireParam(  "cid", "GET", "Esta caja no existe." );
+		$esta_caja = CajaDAO::getByPK( $_GET["cid"] );
+		
+		
+		//
+		// Titulo de la pagina
+		// 
+		$page->addComponent( new TitleComponent( "Detalles de caja: " . $esta_caja->getToken() , 2 ));
+
+		
+		//
+		// Menu de opciones
+		// 
+		$menu = new MenuComponent();
+		$menu->addItem("Editar esta caja", "sucursales.editar.caja.php?cid=".$_GET["cid"]);
+		//$menu->addItem("Desactivar este producto", null);
+                
+                $btn_eliminar = new MenuItem("Desactivar esta caja", null);
+                $btn_eliminar->addApiCall("api/sucursal/caja/eliminar", "GET");
+                $btn_eliminar->onApiCallSuccessRedirect("sucursales.lista.caja.php");
+                $btn_eliminar->addName("eliminar");
+                
+                $funcion_eliminar = " function eliminar_caja(btn){".
+                            "if(btn == 'yes')".
+                            "{".
+                                "var p = {};".
+                                "p.id_caja = ".$_GET["cid"].";".
+                                "sendToApi_eliminar(p);".
+                            "}".
+                        "}".
+                        "      ".
+                        "function confirmar(){".
+                        " Ext.MessageBox.confirm('Desactivar', 'Desea eliminar esta caja?', eliminar_caja );".
+                        "}";
+                
+                $btn_eliminar->addOnClick("confirmar", $funcion_eliminar);
+                
+                $menu->addMenuItem($btn_eliminar);
+                
+		$page->addComponent( $menu);
+		
+		//
+		// Forma de producto
+		// 
+		$form = new DAOFormComponent( $esta_caja );
+		$form->setEditable(false);
+		//$form->setEditable(false);		
+		$form->hideField( array( 
+				"id_caja",
+			 ));
+//		$form->makeObligatory(array( 
+//				"compra_en_mostrador",
+//				"costo_estandar",
+//				"nombre_producto",
+//				"id_empresas",
+//				"codigo_producto",
+//				"metodo_costeo",
+//				"activo"
+//			));
+	    $form->createComboBoxJoin("id_sucursal", "razon_social", SucursalDAO::getAll(), $esta_caja->getIdSucursal() );
+		$page->addComponent( $form );
+		
+		$page->render();
