c7b2e9c1bb3d089a95b46ea50be83f07f4bc4993
Alan Gonzalez <alanboy@alanboy.net>
2011-12-22 07:56:59 +0000

    ya no se pertmite cambiar el id de servicio al momento de crear un nuevo seguimiento de orden de servicio

--- www/front_ends/__instance__/gerencia/servicios.seguimiento.orden.php
+++ www/front_ends/__instance__/gerencia/servicios.seguimiento.orden.php
@@ -8,7 +8,7 @@
 
 		$page = new GerenciaComponentPage();
 
-                //
+		//
 		// Parametros necesarios
 		// 
 		$page->requireParam(  "oid", "GET", "Esta orden de servicio no existe." );
@@ -17,44 +17,30 @@
 		//
 		// Titulo de la pagina
 		// 
-		$page->addComponent( new TitleComponent( "Realizar seguimiento a una orden de servicio ", 2 ));
-
+		$link = "<a href='servicios.detalle.orden.php?oid=". $_GET["oid"] ."'>". $_GET["oid"] ."</a>";
+		$page->addComponent( new TitleComponent( "Seguimiento a la orden de servicio " . $link, 2 ));
+		
+		
 		//
 		// Forma de usuario
 		// 
 		$form = new DAOFormComponent( new SeguimientoDeServicio() );
 		$form->hideField( array( 
-                                "id_seguimiento_de_servicio",
-                                "id_usuario",
-                                "id_sucursal",
-                                "fecha_seguimiento"
-                                
-			 ));
+                "id_seguimiento_de_servicio",
+                "id_usuario",
+                "id_sucursal",
+                "fecha_seguimiento"               
+		));
+        
+		$form->sendHidden( "id_orden_de_servicio" );
+        
+		$form->addApiCall( "api/servicios/orden/seguimiento/", "GET" );
+
                 
-                $form->addApiCall( "api/servicios/orden/seguimiento/", "GET" );
+        $form->createComboBoxJoin( "id_orden_de_servicio", "id_orden_de_servicio", OrdenDeServicioDAO::getAll(), $esta_orden->getIdOrdenDeServicio() );
                 
-//                $form->addField("id_producto", "Productos", "text","","productos");
-//                $form->createListBoxJoin("id_producto", "nombre_producto", ProductoDAO::search( new Producto( array( "activo" => 1 ) ) ));
-//
-                
-                $form->createComboBoxJoin( "id_orden_de_servicio", "id_orden_de_servicio", OrdenDeServicioDAO::getAll(), $esta_orden->getIdOrdenDeServicio() );
-                
-                $form->createComboBoxJoinDistintName( "id_localizacion", "id_sucursal" , "razon_social", SucursalDAO::search(new Sucursal( array( "activa" => 1 ) )) );
-                
-//                $form->createComboBoxJoin( "id_rol", "nombre", RolDAO::getAll(), $este_usuario->getIdRol() );
-//
-//                $form->createComboBoxJoin( "id_moneda", "nombre", MonedaDAO::search( new Moneda( array( "activa" => 1 ) ) ),$este_usuario->getIdMoneda() );
-//
-//                $form->createComboBoxJoin( "id_clasificacion_cliente", "nombre", ClasificacionClienteDAO::getAll(), $este_usuario->getIdClasificacionCliente() );
-//
-//                $form->createComboBoxJoin( "id_clasificacion_proveedor", "nombre", ClasificacionProveedorDAO::search( new ClasificacionProveedor( array( "activa" => 1 ) ) ), $este_usuario->getIdClasificacionProveedor() );
-            
-                //$form->createComboBoxJoin("id_tipo_almacen", "descripcion", array_diff(TipoAlmacenDAO::getAll(), TipoAlmacenDAO::search( new TipoAlmacen( array( "id_tipo_almacen" => 2 ) ) ) ), $este_almacen->getIdTipoAlmacen() );
-                
-//                $form->renameField( array( 
-//                    "id_ciudad" => "municipio"
-//		));
-                
+        $form->createComboBoxJoinDistintName( "id_localizacion", "id_sucursal" , "razon_social", SucursalDAO::search(new Sucursal( array( "activa" => 1 ) )) );
+
 		$page->addComponent( $form );
                 
 		$page->render();
