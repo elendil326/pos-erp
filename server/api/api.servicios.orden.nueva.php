acce73167f84d9723d2c6c3944adad55f44c44fb
Andres Zendejas <panda326@gmail.com>
2011-12-27 02:25:50 +0000

    arreglado orden de parametros para nueva orden

--- server/api/api.servicios.orden.nueva.php
+++ server/api/api.servicios.orden.nueva.php
@@ -17,10 +17,10 @@
 	protected function GetRequest()
 	{
 		$this->request = array(	
-			"id_cliente" => new ApiExposedProperty("id_cliente", true, POST, array( "int" )),
+			"descripcion" => new ApiExposedProperty("descripcion", true, POST, array( "string" )),
 			"id_servicio" => new ApiExposedProperty("id_servicio", true, POST, array( "int" )),
+			"id_cliente" => new ApiExposedProperty("id_cliente", true, POST, array( "int" )),
 			"fecha_entrega" => new ApiExposedProperty("fecha_entrega", true, POST, array( "string" )),
-			"descripcion" => new ApiExposedProperty("descripcion", false, POST, array( "string" )),
 			"adelanto" => new ApiExposedProperty("adelanto", false, POST, array( "float" )),
 		);
 	}
@@ -30,10 +30,10 @@
  		$this->response = ServiciosController::NuevaOrden( 
  			
 			
-			isset($_POST['id_cliente'] ) ? $_POST['id_cliente'] : null,
-			isset($_POST['id_servicio'] ) ? $_POST['id_servicio'] : null,
-			isset($_POST['fecha_entrega'] ) ? $_POST['fecha_entrega'] : null,
 			isset($_POST['descripcion'] ) ? $_POST['descripcion'] : null,
+			isset($_POST['id_servicio'] ) ? $_POST['id_servicio'] : null,
+			isset($_POST['id_cliente'] ) ? $_POST['id_cliente'] : null,
+			isset($_POST['fecha_entrega'] ) ? $_POST['fecha_entrega'] : null,
 			isset($_POST['adelanto'] ) ? $_POST['adelanto'] : null
 			
 			);
