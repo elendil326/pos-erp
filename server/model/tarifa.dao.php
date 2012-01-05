e6f6354799669e4a5980dc547d76c000f2346116
Andres Zendejas <panda326@gmail.com>
2012-01-05 00:25:18 +0000

    metodo de obtenerTarifa

--- server/model/tarifa.dao.php
+++ server/model/tarifa.dao.php
@@ -21,4 +21,58 @@ require_once("base/tarifa.vo.base.php");
 class TarifaDAO extends TarifaDAOBase
 {
 
+        public static function obtenerTarifa()
+        {
+            //Se obtienen todas las tarifas activas son su version activa, su version default, y la
+            //fecha de inicio y de fin de la version activa.
+            $sql = "select t.id_tarifa, t.id_version_activa, t.id_version_default, v.fecha_inicio, 
+                v.fecha_fin from tarifa t, version v where t.activa = 1 and v.id_version=t.id_version_activa";
+            
+            global $conn;
+            $rs = $conn->Execute($sql);
+            $tarifas_versiones = array();
+            
+            //Recorremos el resultado y guardamos el id de la version que se va a usar, si la version activa
+            //ya caduco o aun no se activa entonces se toma la default.
+            foreach($rs as $result)
+            {
+                $tarifa_version = array();
+                $tarifa_version["id_tarifa"] = $result["id_tarifa"];
+                
+                if(is_null($result["fecha_inicio"]) || is_null($result["fecha_fin"]))
+                {
+                    $tarifa_version["id_version"] = $result["id_version_activa"];
+                    continue;
+                }
+                
+                $fecha_inicio = strtotime($result["fecha_inicio"]);
+                $fecha_fin = strtotime($result["fecha_fin"]);
+                
+                $now = time();
+                
+                if($fecha_inicio>$now&&$now<$fecha_fin)
+                {
+                    $tarifa_version["id_version"] = $result["id_version_activa"];
+                }
+                else
+                {
+                    $tarifa_version["id_version"] = $result["id_version_default"];
+                }
+                array_push($tarifas_versiones,$tarifa_version);
+            }
+            
+            //Una vez que tenemos los ids de las tarifas y de las versiones, traemos
+            //todas las reglas de la version y las guardamos en el arreglo final
+            $reglas = array();
+            
+            foreach($tarifas_versiones as $tarifa_version)
+            {
+                $regla = array();
+                $regla["id_tarifa"] = $tarifa_version["id_tarifa"];
+                $regla["reglas"] = ReglaDAO::search( new Regla( array( "id_version" => $tarifa_version["id_version"] ) ) , "secuencia");
+                array_push($reglas,$regla);
+            }
+            
+            return $reglas;
+        }
 }
