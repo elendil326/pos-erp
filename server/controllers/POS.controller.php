577ef2c7e097ee7fe9fed6ba630339d65ffdcde6
Alan Gonzalez <alanboy@alanboy.net>
2012-01-06 01:37:36 +0000

    vaciar ciudades e insertar ciudades a partir de id 1

--- server/controllers/POS.controller.php
+++ server/controllers/POS.controller.php
@@ -129,8 +129,41 @@ require_once("interfaces/POS.interface.php");
 
   		$conn->Execute( "TRUNCATE TABLE `unidad`; "  );
 
+  		$conn->Execute( "TRUNCATE TABLE `ciudad`; "  );
+
   		$conn->Execute( "INSERT INTO `usuario` (`id_usuario`, `id_direccion`, `id_direccion_alterna`, `id_sucursal`, `id_rol`, `id_clasificacion_cliente`, `id_clasificacion_proveedor`, `id_moneda`, `fecha_asignacion_rol`, `nombre`, `rfc`, `curp`, `comision_ventas`, `telefono_personal1`, `telefono_personal2`, `fecha_alta`, `fecha_baja`, `activo`, `limite_credito`, `descuento`, `password`, `last_login`, `consignatario`, `salario`, `correo_electronico`, `pagina_web`, `saldo_del_ejercicio`, `ventas_a_credito`, `representante_legal`, `facturar_a_terceros`, `dia_de_pago`, `mensajeria`, `intereses_moratorios`, `denominacion_comercial`, `dias_de_credito`, `cuenta_de_mensajeria`, `dia_de_revision`, `codigo_usuario`, `dias_de_embarque`, `tiempo_entrega`, `cuenta_bancaria`) VALUES (1, NULL, NULL, NULL, 0, NULL, NULL, NULL, '2011-10-24 18:28:24', 'Alan Gonzalez', NULL, NULL, NULL, NULL, NULL, '2011-10-24 18:28:34', NULL, 1, 0, NULL, '202cb962ac59075b964b07152d234b70', NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, NULL);") ;
 		
+		$conn->Execute( "INSERT INTO `ciudad` (`id_ciudad`, `id_estado`, `nombre`) VALUES
+		(1, 11, 'Abasolo'),
+		(2, 11, 'Acambaro'),
+		(3, 11, 'Apaseo el Alto'),
+		(4, 11, 'Apaseo el Grande'),
+		(5, 11, 'Atarjea'),
+		(6, 11, 'Celaya'),
+		(7, 11, 'Comonfort'),
+		(8, 11, 'Coroneo'),
+		(9, 11, 'Cortazar'),
+		(10, 11, 'Cueramaro'),
+		(11, 11, 'Doctor Mora'),
+		(12, 11, 'Dolores Hidalgo'),
+		(13, 11, 'Guanajuato'),
+		(14, 11, 'Huanimaro'),
+		(15, 11, 'Irapuato'),
+		(16, 11, 'Jaral del Progreso'),
+		(17, 11, 'Jerecuaro'),
+		(18, 11, 'LeÃ³n'),
+		(19, 11, 'Manuel Doblado'),
+		(20, 11, 'Moroleon'),
+		(21, 11, 'Ocampo'),
+		(22, 11, 'Penjamo'),
+		(23, 11, 'Pueblo Nuevo'),
+		(24, 11, 'PurÃ­sima del Rincon'),
+		(25, 11, 'Romita'),
+		(26, 11, 'Salamanca'),
+		(27, 11, 'Salvatierra'),
+		(28, 11, 'San Diego de la Union'),
+		(29, 11, 'San Felipe'),
+		(30, 11, 'San Francisco del Rincon');");
 
 
 	}
