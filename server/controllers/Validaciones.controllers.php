61ce3e0040f46cdb1d5615b428560d9e2df1ce52
Alan Gonzalez <alanboy@alanboy.net>
2012-01-29 02:48:58 +0000

    agregando controller de validaciones

---
+++ server/controllers/Validaciones.controllers.php
@@ -0,0 +1,59 @@
+<?php
+
+
+
+class ValidacionesController {
+	
+
+	/*
+	 * Validar que $a sea una fecha valida en unix
+	 *
+	 */
+	public static function validarFechaUnix($a){
+		
+	}
+
+
+	/*
+     *
+     * Validar que $a sea una fehca valida que sea 
+     * despues de justo ahora con por lo menos
+     * una holgura de $b. Si $b es null, se calculara
+     * con una holgura default de 1 dia.
+     *
+     */
+    public static function validarFechaUnixEnFuturo($a, $b){
+    	
+    }
+
+
+    /*
+     * Valida que la longitud de la cadena $a, sea entre
+     * $i y $j. 
+     *
+     *
+     */
+    public static function validarLongitudDeCadena($a, $i, $j){
+    	
+    }
+
+
+    /*
+     *
+     * Validar que un numero $n sea entero y este entre el rango
+     * de $i y $j
+     *
+     */
+     public static function validarEntero($n, $i, $j){
+     	
+     }
+
+
+     /*
+      *
+      *
+      *
+      *
+      */
+      
+}
--- tests/ClientesControllerTests.php
+++ tests/ClientesControllerTests.php
@@ -2,3 +2,4 @@
 	class ClientesControllerTests extends PHPUnit_Framework_TestCase {
 		
 	}
+
