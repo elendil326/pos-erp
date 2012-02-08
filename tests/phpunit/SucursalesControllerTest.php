f2af7d540f05e1e2a7674a69f572dc40d3a6e531
Alan Gonzalez <alanboy@alanboy.net>
2012-02-08 21:48:34 +0000

    agregando tests de sucursales

---
+++ tests/phpunit/SucursalesControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+date_default_timezone_set ( "America/Mexico_City" );
+
+define("BYPASS_INSTANCE_CHECK", false);
+
+$_GET["_instance_"] = 123;
+
+require_once("../../server/bootstrap.php");
+
+
+
+
+
+class SucursalesControllerTest extends PHPUnit_Framework_TestCase {
+	
+	protected function setUp(){
+		Logger::log("-----------------------------");
+		$r = SesionController::Iniciar(123, 1, true);
+	}
+	
+}
