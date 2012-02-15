d1c286ace4f27e769c9ec9409b6ca547663c474c
Alan Gonzalez <alanboy@alanboy.net>
2012-02-15 15:57:45 +0000

    restaurando config.deafult.php y arreglando conflicto

--- www/front_ends/__instance__/sucursal/index.php
+++ www/front_ends/__instance__/sucursal/index.php
@@ -3,17 +3,80 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-    <link rel="stylesheet" href="http://cdn.labs.caffeina.mx/sencha-touch-2.0.0-pr3/resources/css/sencha-touch.css" type="text/css">
+    <link rel="stylesheet" href="http://docs.sencha.com/touch/2-0/touch/resources/css/sencha-touch.css" type="text/css">
     <link rel="stylesheet" href="test.css" type="text/css">
-    <script type="text/javascript" src="http://cdn.labs.caffeina.mx/sencha-touch-2.0.0-pr3/sencha-touch-all.js"></script>
+    <script type="text/javascript" src="http://docs.sencha.com/touch/2-0/touch/sencha-touch.js"></script>
+	
+    <script type="text/javascript" src="base.js" charset="utf-8"></script>
 	<script type="text/javascript" src="test.js" charset="utf-8"></script>
 
 
 	<script type="text/javascript">
+Ext.Loader.setPath({
+    'Ext.data': '../../src/data',
+    'Kitchensink': 'app'
+});
+
+//NOTE: This will not be necessary after beta 1 and is only present temporarily to enable built
+// (non-dynamic loading) versions of the kitchen sink to work on tablet and phone devices
+Ext.require([
+    'Kitchensink.view.phone.Main',
+    'Kitchensink.view.phone.TouchEvents',
+    'Kitchensink.view.tablet.Main',
+    'Kitchensink.view.tablet.TouchEvents',
+    'Kitchensink.controller.phone.Main',
+    'Kitchensink.controller.tablet.Main'
+]);
+
 Ext.application({
     name: 'Kitchensink',
-    controllers: ['Main'],
-    models: ['Demo']
+
+    icon: 'resources/img/icon.png',
+    tabletStartupScreen: 'resources/img/tablet_startup.png',
+    phoneStartupScreen: 'resources/img/phone_startup.png',
+
+
+    views: [
+        'NestedList',
+        'List',
+        'SourceOverlay',
+        'Buttons',
+        'Forms',
+        'Icons',
+        'BottomTabs',
+        'Themes',
+        'Map',
+        'Overlays',
+        'Tabs',
+        'Toolbars',
+        'JSONP',
+        'YQL',
+        'Ajax',
+        'Video',
+        'Audio',
+        'NestedLoading',
+        'Carousel',
+        'TouchEvents',
+        'SlideLeft',
+        'SlideRight',
+        'SlideUp',
+        'SlideDown',
+        'CoverLeft',
+        'CoverRight',
+        'CoverUp',
+        'CoverDown',
+        'RevealLeft',
+        'RevealRight',
+        'RevealUp',
+        'RevealDown',
+        'Pop',
+        'Fade',
+        'Flip',
+        'Cube'
+    ],
+
+    stores: ['Demos'],
+    profiles: ['Tablet', 'Phone']
 });
 	</script>
 </head>
