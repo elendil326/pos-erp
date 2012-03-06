a00b1dee67d529a2e71aee0b66f7e983b99c2ccc
Juan Manuel Hernandez <figu05@gmail.com>
2012-03-06 02:51:50 +0000

    Funciones nuevas en EmpresasControllerTest

--- tests/phpunit/EmpresasControllerTest.php
+++ tests/phpunit/EmpresasControllerTest.php
@@ -73,7 +73,94 @@ class EmpresasControllerTest extends PHPUnit_Framework_TestCase {
 		$this->assertInternalType('int', $this->_empresa["id_empresa"] );
 	}
 
+	/**
+    * @expectedException BusinessLogicException
+    */
+	public function testNuevoEmpresaMismoNombre(){
+		$direccion = Array(
+			"calle"  			=> "Calle ".time(),
+	        "numero_exterior"   => "107",
+	        "colonia"  			=> "Colonia ".time(),
+	        "id_ciudad"  		=> 334,
+	        "codigo_postal"  	=> "38060",
+	        "numero_interior"  	=> null,
+	        "texto_extra"  		=> null,
+	        "telefono1"  		=> "4616149974",
+	        "telefono2"			=> "45*451*454"
+		);
+		
+		$id_moneda = 1;
+		$razon_social = "Caffeina Software".time();
+		$rfc  = "GOHA".time();
+		
+		$empresas_con_rfc = EmpresaDAO::getByRFC($rfc);
+		
+		foreach ($empresas_con_rfc as $empObj) {
+			EmpresasController::Eliminar( $empObj->getIdEmpresa() );
+		}
 
+		$this->_empresa = EmpresasController::Nuevo(
+				array($direccion), 
+				$id_moneda, 
+				$razon_social, 
+				$rfc
+			);
+
+		$this->assertInternalType('int', $this->_empresa["id_empresa"] );
+		//se trata de ingresar una empresa con los mismos datos
+		$this->_empresa = EmpresasController::Nuevo(
+				array($direccion), 
+				$id_moneda, 
+				$razon_social, 
+				$rfc
+			);
+	}
+
+	public function testEditarEmpresa(){
+		$direccion = Array(
+			"calle"  			=> "Calle ".time(),
+	        "numero_exterior"   => "107",
+	        "colonia"  			=> "Colonia ".time(),
+	        "id_ciudad"  		=> 334,
+	        "codigo_postal"  	=> "38060",
+	        "numero_interior"  	=> null,
+	        "texto_extra"  		=> null,
+	        "telefono1"  		=> "4616149974",
+	        "telefono2"			=> "45*451*454"
+		);
+		
+		$id_moneda = 1;
+		$razon_social = "Caffeina Software-".time();
+		$rfc  = "GOHA-".time();
+
+		$nueva_empresa = EmpresasController::Nuevo(
+				array($direccion), 
+				$id_moneda, 
+				$razon_social, 
+				$rfc
+			);
+
+		$this->assertInternalType('int', $nueva_empresa["id_empresa"] );
+
+		$original = EmpresaDAO::getByPK( $nueva_empresa['id_empresa'] );
+		//se edita la empresa con los mismos datos
+		EmpresasController::Editar($id_empresa = $nueva_empresa['id_empresa'], 
+									$cedula = "cedula_".time(), //se cambia
+									$direccion = null, 
+									$email = null, 
+									$id_moneda = null, 
+									$impuestos_venta = null, 
+									$impuesto_compra = null, 
+									$logo = null, 
+									$razon_social = null, 
+									$representante_legal = null, 
+									$texto_extra = "Texto_".time());//se cambia
+
+		$editada = EmpresaDAO::getByPK( $nueva_empresa['id_empresa'] );
+		
+		$this->assertNotEquals($editada->getCedual() , $original->getCedula(),"---- 'testEditarEmpresa' LA CEDULA NO SE ACTUALIZÓ");
+		$this->assertNotEquals($editada->getTextoExtra() , $original->getTextExtra(),"---- 'testEditarEmpresa' LA TEXTO EXTRA NO SE ACTUALIZÓ");
+	}
 
 	
 	public function testBuscar(){
