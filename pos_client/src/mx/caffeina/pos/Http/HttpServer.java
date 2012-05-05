25767f4cb5d08fc3ddae302b536b0ed1262f99a7
Alan Gonzalez <alanboy@alanboy.net>
2012-05-05 22:36:43 +0000

    OMFG, refactor tambien de HttpServer, no sabia que java1.6 ya tenia uno. y con todo y HTTPS. NICE, por lo pronto solo funciona por https, y el dispatcher es el mismo que usaba antes

--- pos_client/src/mx/caffeina/pos/Http/HttpServer.java
+++ pos_client/src/mx/caffeina/pos/Http/HttpServer.java
@@ -1,5 +1,182 @@
 package mx.caffeina.pos.Http;
 
+import com.sun.net.httpserver.*;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.security.KeyStore;
+import javax.net.ssl.*;
+import java.util.concurrent.*;
+import java.net.URL;
+
+
+import mx.caffeina.pos.*;
+
+
+
+
+/**
+  * http://neopatel.blogspot.mx/2010/05/java-comsunnethttpserverhttpserver.html
+  * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6563368
+  * keytool -genkey -alias mydomain -keyalg RSA -keystore keystore.jks -keysize 2048
+  *
+  **/
+
+
+class PosClientHttpsHandler implements HttpHandler {
+
+
+
+       public void handle(HttpExchange t) throws IOException {
+			
+			InputStream is = t.getRequestBody();
+
+			int c;
+			while((c= is.read()) != -1){
+				System.out.print((char)c);
+			}
+
+			Headers h = t.getRequestHeaders();
+
+			String host = h.get("Host").toString().replace('[', ' ' ).replace(']', ' ').trim();
+
+			
+			//System.out.println( h.get("Cookie"));
+			//System.out.println( h.get("Content-Type"));
+			//System.out.println( h.get("User-Agent"));
+			//System.out.println( h.get("Accept"));
+			//System.out.println( h.get("Accept-Charset"));
+			//System.out.println(t.getRemoteAddress());
+			//t.getRequestMethod()
+
+			
+
+
+			URL url = new URL("https://" + host.trim() + t.getRequestURI().toString().trim());
+			
+			System.out.println(url);
+
+			HttpResponder response = null;
+
+			Dispatcher d = new Dispatcher();
+			response = d.dispatch( url );
+
+			t.sendResponseHeaders(200, response.getResponse().length());
+
+			OutputStream os = t.getResponseBody();
+			
+			os.write(response.getResponse().getBytes());
+			os.close();
+       }
+}
+   
+
+
+public class HttpServer{
+	
+	public HttpServer()  {
+		 new HttpServer( 16001 );
+	}
+
+    public HttpServer(int port){ 
+
+		HttpsServer server = null;
+
+		try{
+			server = HttpsServer.create(new InetSocketAddress(port), 5);
+		}catch(IOException ioe){
+			Logger.error("Imposible crear el socket:" + ioe);
+			return;
+		}
+
+		char[] passphrase = "passphrase".toCharArray();
+
+		KeyStore ks;
+		KeyManagerFactory kmf ;
+		TrustManagerFactory tmf ;
+		SSLContext ssl;
+
+
+		try{
+			ks = KeyStore.getInstance("JKS");
+			ks.load( new FileInputStream("testkeys"), passphrase);
+			kmf = KeyManagerFactory.getInstance("SunX509");	
+			kmf.init(ks, passphrase);
+			tmf = TrustManagerFactory.getInstance("SunX509");
+			tmf.init(ks);
+
+			ssl = SSLContext.getInstance("TLS");
+			ssl.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+		}catch(java.security.KeyStoreException e){
+			Logger.error(e);
+			return;
+
+		}catch(java.security.KeyManagementException e){
+			Logger.error(e);
+			return;
+
+		}catch(java.security.NoSuchAlgorithmException e ){
+			Logger.error(e);
+			return;
+
+		}catch(java.security.UnrecoverableKeyException e){
+			Logger.error(e);
+			return;
+
+		}catch(java.security.cert.CertificateException e){
+			Logger.error(e);
+			return;
+
+		}catch(FileNotFoundException fnfe){
+			Logger.error(fnfe);
+			return;
+
+		}catch(java.io.IOException ioe){
+			Logger.error(ioe);
+			return;
+
+		}
+		
+
+
+		server.setHttpsConfigurator (new HttpsConfigurator((SSLContext)ssl) {
+
+				public void configure (HttpsParameters params) {
+
+				// get the remote address if needed
+				InetSocketAddress remote = params.getClientAddress();
+
+				SSLContext c = getSSLContext();
+
+				// get the default parameters
+				SSLParameters sslparams = c.getDefaultSSLParameters();
+
+				/*if (remote.equals (...) ) {
+				// modify the default set for client x
+				}*/
+
+				params.setSSLParameters(sslparams);
+				// statement above could throw IAE if any params invalid.
+				// eg. if app has a UI and parameters supplied by a user.
+				
+			}
+		});
+
+		server.createContext("/", new PosClientHttpsHandler());
+
+		server.setExecutor(Executors.newFixedThreadPool(50)); // creates a default executor
+
+		System.out.println("iniciando....");
+
+		server.start();	
+	}
+}
+
+
+
+/*
+package mx.caffeina.pos.Http;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -13,11 +190,7 @@ import java.net.URL;
 
 
 
-/**
-  * 
-  * 
-  * 
-  **/
+
 public class HttpServer 
 {         
 
@@ -265,3 +438,4 @@ public class HttpServer
 
 
 
+*/
