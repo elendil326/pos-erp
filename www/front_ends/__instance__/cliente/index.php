bc45376769c50e2ed66710484109a089f7b30277
Alan Gonzalez <alanboy@alanboy.net>
2012-06-12 04:15:34 +0000

    lista de ordenes de servicio

--- www/front_ends/__instance__/cliente/index.php
+++ www/front_ends/__instance__/cliente/index.php
@@ -10,6 +10,35 @@
 	$page = new ClienteComponentPage();
 	$banner = new BannerComponent("POS ERP", "Bienvenido a POS ERP <br>un sistema de gestion empresarial", "../../../media/EAbydW1M_XR.png");
 	$page->addComponent($banner);
+	
+	
+	//buscar si tiene ordenes de servico a su nombre
+	$sesion_actual = SesionController::Actual();
+	
+	
+	$ordenes_array = new OrdenDeServicio( array("id_usuario_venta" => $sesion_actual["id_usuario"]));
+	
+	$ordenes = OrdenDeServicioDAO::search( $ordenes_array );
+	
+	if(sizeof($ordenes) > 0 ){
+		$page->addComponent("Sus ordenes");
+		
+		$table = new TableComponent(array(
+			"activa" => "activa",
+			"id_servicio" => "id_servicio",
+			"fecha_orden" => "fecha de Orden"
+		), $ordenes);
+		
+		function renderIdServicio($id){
+			$serv = ServicioDAO::getByPK($id);
+			return $serv->getNombreServicio();
+		}
+		
+		$table->addColRender("id_servicio", "renderIdServicio");
+		$page->addComponent($table);
+	}
+	
+	
 	$page->render();
 
 
