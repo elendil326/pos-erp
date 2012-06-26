3d8c846e0d3c6c5c246feb65f834a30daa6ace22
Alan Gonzalez <alanboy@alanboy.net>
2012-06-26 19:43:05 +0000

    Avances al SDK de php.

--- sdk/php/pos-erp-sdk.php
+++ sdk/php/pos-erp-sdk.php
@@ -1,66 +1,128 @@
 <?php
 
-function post_request($url, $data, $referer='') {
- 
-    // Convert the data array into URL Parameters like a=b&foo=bar etc.
-    $data = http_build_query($data);
- 
-    // parse the given URL
-    $url = parse_url($url);
- 
-    if ($url['scheme'] != 'http') { 
-        die('Error: Only HTTP request are supported !');
+
+Class PosErp{
+
+    private static $singleton_obj;
+    private $waiting;
+    private $instance_token;
+    private $url;
+
+
+    private function __construct(){
+        $this->waiting = false;
+        $this->instance_token = null;
+        $this->url = "asdf";
     }
- 
-    // extract host and path:
-    $host = $url['host'];
-    $path = $url['path'];
- 
-    // open a socket connection on port 80 - timeout: 30 sec
-    $fp = fsockopen($host, 80, $errno, $errstr, 30);
- 
-    if ($fp){
- 
-        // send the request headers:
-        fputs($fp, "POST $path HTTP/1.1\r\n");
-        fputs($fp, "Host: $host\r\n");
- 
-        if ($referer != '')
-            fputs($fp, "Referer: $referer\r\n");
- 
-        fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
-        fputs($fp, "Content-length: ". strlen($data) ."\r\n");
-        fputs($fp, "Connection: close\r\n\r\n");
-        fputs($fp, $data);
- 
-        $result = ''; 
-        while(!feof($fp)) {
-            // receive the results of the request
-            $result .= fgets($fp, 128);
+
+    function __clone(){}
+
+    function __destroy(){}
+
+
+
+
+    public static function getInstance(){
+        if(self::$singleton_obj === null){
+            self::$singleton_obj = new PosErp();
         }
+
+        return self::$singleton_obj;  
     }
-    else { 
+
+
+    public function setInstance($token){
+        $this->instance_token = $token;
+    }
+
+
+    private function postRequest($url, $data, $referer='') {
+     
+        // Convert the data array into URL Parameters like a=b&foo=bar etc.
+        $data = http_build_query($data);
+     
+        // parse the given URL
+        $url = parse_url($url);
+     
+        if ($url['scheme'] != 'http') { 
+            die('Error: Only HTTP request are supported !');
+        }
+     
+        // extract host and path:
+        $host = $url['host'];
+        $path = $url['path'];
+     
+        // open a socket connection on port 80 - timeout: 30 sec
+        $fp = fsockopen($host, 80, $errno, $errstr, 30);
+     
+        if ($fp){
+     
+            // send the request headers:
+            fputs($fp, "POST $path HTTP/1.1\r\n");
+            fputs($fp, "Host: $host\r\n");
+     
+            if ($referer != '')
+                fputs($fp, "Referer: $referer\r\n");
+     
+            fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
+            fputs($fp, "Content-length: ". strlen($data) ."\r\n");
+            fputs($fp, "Connection: close\r\n\r\n");
+            fputs($fp, $data);
+     
+            $result = ''; 
+            while(!feof($fp)) {
+                // receive the results of the request
+                $result .= fgets($fp, 128);
+            }
+        }
+        else { 
+            return array(
+                'status' => 'err', 
+                'error' => "$errstr ($errno)"
+            );
+        }
+     
+        // close the socket connection:
+        fclose($fp);
+     
+        // split the result header from the content
+        $result = explode("\r\n\r\n", $result, 2);
+     
+        $header = isset($result[0]) ? $result[0] : '';
+        $content = isset($result[1]) ? $result[1] : '';
+     
+        // return as structured array:
         return array(
-            'status' => 'err', 
-            'error' => "$errstr ($errno)"
+            'status' => 'ok',
+            'header' => $header,
+            'content' => $content
         );
     }
- 
-    // close the socket connection:
-    fclose($fp);
- 
-    // split the result header from the content
-    $result = explode("\r\n\r\n", $result, 2);
- 
-    $header = isset($result[0]) ? $result[0] : '';
-    $content = isset($result[1]) ? $result[1] : '';
- 
-    // return as structured array:
-    return array(
-        'status' => 'ok',
-        'header' => $header,
-        'content' => $content
-    );
+
+
+    private function ApiCall($method, $address){
+
+        if(is_null($this->instance_token)){
+            throw new Exception("You have not supplied any instance token", 1);
+        }
+
+        if($method === "POST"){
+
+        }else if($method === "GET"){
+
+        }else{
+            throw new Exception("Http methdos must be POST or GET", 1);
+            
+        }
+
+    }
+
+
+
+    public function NuevoCliente($uno, $dos){
+
+    }
+
 }
 
 
@@ -69,6 +131,16 @@ function post_request($url, $data, $referer='') {
 
 
 
+
+
+
+$api = PosErp::getInstance("aslkdfjaslkdfjlkasjdfs");
+$api->SesionIniciar(23, 425435);
+$api->NuevoCliente(2,4);
+
+
+
+exit;
 
 
 
@@ -135,7 +207,7 @@ if ($result['status'] != 'ok'){
 
 
 
-
+/*
 var obj = {
 		razon_scocial 		: $("#rs"),
 		representante_legal	: $("#rl"),
@@ -161,3 +233,4 @@ $.ajax({
 });
 
 
+*/
