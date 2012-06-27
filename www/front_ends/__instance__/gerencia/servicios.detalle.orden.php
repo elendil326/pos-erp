e1273e7c04fa3576097296076b67766228474724
Alan Gonzalez <alanboy@alanboy.net>
2012-06-27 03:24:03 +0000

    bit of gui

--- www/front_ends/__instance__/gerencia/servicios.detalle.orden.php
+++ www/front_ends/__instance__/gerencia/servicios.detalle.orden.php
@@ -57,21 +57,18 @@ if ( $esta_orden->getActiva() ){
 	$menu->addMenuItem($btn_eliminar);
 
 
-
-	$btn_terminar = new MenuItem("Terminar orden", null);
-	$btn_terminar->addName("terminar");
-	$funcion_terminar = " function redir_terminar( ){ window.location = \"servicios.terminar.orden.php?oid=" . $_GET["oid"] . "\"; } ";
-	$btn_terminar->addOnClick("redir_terminar", $funcion_terminar);
-	$menu->addMenuItem($btn_terminar);
-
-
-
-	
 	$editar = new MenuItem("Editar orden", null);
 	$editar->addOnClick("_e", "function _e(){ window.location = 'servicios.detalle.orden.editar.php?oid=". $_GET["oid"] ."' ; }");
 	$menu->addMenuItem($editar);
 	
 
+
+	$btn_terminar = new MenuItem("Terminar orden", null);
+	$btn_terminar->addName("terminar");
+	$btn_terminar->addClass("OK");
+	$funcion_terminar = " function redir_terminar( ){ window.location = \"servicios.terminar.orden.php?oid=" . $_GET["oid"] . "\"; } ";
+	$btn_terminar->addOnClick("redir_terminar", $funcion_terminar);
+	$menu->addMenuItem($btn_terminar);
 }
 
 $imp = new MenuItem(" <img src='../../../media/iconos/printer.png'> Imprimir", null);
