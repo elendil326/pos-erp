6e10e970e5b34abd43cccf65bc22fa831848c59d
Alan Gonzalez <alanboy@alanboy.net>
2012-07-11 21:43:53 +0000

    bug fix al editar documentos

--- www/__instance__/gerencia/documentos.editar.php
+++ www/__instance__/gerencia/documentos.editar.php
@@ -13,6 +13,14 @@
 		
 
 		$page->addComponent(new TitleComponent( $q->getNombre(),2));
+
+
+		$q->setJsonImpresion( str_replace ( "\\n" , "" , $q->getJsonImpresion() ) );
+		$q->setJsonImpresion( str_replace ( "\\t" , "" , $q->getJsonImpresion() ) );
+		$q->setJsonImpresion( stripslashes($q->getJsonImpresion())  );
+		$q->setJsonImpresion( substr($q->getJsonImpresion(), 1 , -1) );
+
+
 		$tabla = new DAOFormComponent( $q );
 		$tabla->setEditable(true);
 		$tabla->renameField(array("id_documento_base" => "id_documento"));
