7526b3e6d9ebcd5c6bdf8fe5c6772e922c352303
Juan Manuel Garcia <zonademanel@gmail.com>
2012-08-03 19:45:16 +0000

    fix en salida lote, se hacia una comparacion de unidades qeu estaba mal, se creo un metodo qeu calucula las existencias en un lote en especifico, lo que habia programado no lo calcula correctamente

--- server/model/producto.dao.php
+++ server/model/producto.dao.php
@@ -4,77 +4,74 @@ require_once ('Estructura.php');
 require_once("base/producto.dao.base.php");
 require_once("base/producto.vo.base.php");
 /** Page-level DocBlock .
-  * 
-  * @author Andres
-  * @package docs
-  * 
-  */
+ * 
+ * @author Andres
+ * @package docs
+ * 
+ */
+
 /** Producto Data Access Object (DAO).
-  * 
-  * Esta clase contiene toda la manipulacion de bases de datos que se necesita para 
-  * almacenar de forma permanente y recuperar instancias de objetos {@link Producto }. 
-  * @author Andres
-  * @access public
-  * @package docs
-  * 
-  */
-class ProductoDAO extends ProductoDAOBase
-{
+ * 
+ * Esta clase contiene toda la manipulacion de bases de datos que se necesita para 
+ * almacenar de forma permanente y recuperar instancias de objetos {@link Producto }. 
+ * @author Andres
+ * @access public
+ * @package docs
+ * 
+ */
+class ProductoDAO extends ProductoDAOBase {
 
-	public static function buscarProductos($query, $how_many = 100){
-		
-		
-		/*$sql = "select * from producto where ( nombre_producto like ? or codigo_producto like ? ) and activo = 1 limit ?; ";
-		$val = array( "%" . $query . "%" , "%" . $query . "%" , $how_many );*/
-		
-		
-		
-		$parts = explode(" ", $query);
-
-		$sql = "select * from producto where (";
-		$val = array();
-		$first = true;
-		foreach ($parts as $p) {
-			if($first){
-				$first = false;
-				
-			}else{
-				$sql .= " and ";
-			}
-			$sql .= "  nombre_producto like ? ";
-			array_push($val , "%" . $p . "%");
-		}
-		
-		$sql .= " or codigo_producto like ? ) limit 20 ";
-		array_push($val , "%" . $query . "%");
-		
-		global $conn;
-		$rs = $conn->Execute($sql, $val);
-		$ar = array( );
-		foreach ($rs as $foo) {
-			$bar =  new Producto($foo);
-    		array_push( $ar,$bar);
-		}
-		return $ar;
-	}
-  
-
-	public static function ExistenciasTotales($id_producto){
-
-		$lotes = LoteProductoDAO::search( new LoteProducto( array( "id_producto" => $id_producto ) ) );
-		
-		$total = 0;
-
-		foreach($lotes as $l ){
-			$total += $l->getCantidad();
-		}
-
-		return $total;
-	}
+    public static function buscarProductos($query, $how_many = 100) {
 
 
-	public static function buscarProductoEnSucursal($id_producto, $id_sucursal){
-			$sql = "SELECT
+        /* $sql = "select * from producto where ( nombre_producto like ? or codigo_producto like ? ) and activo = 1 limit ?; ";
+          $val = array( "%" . $query . "%" , "%" . $query . "%" , $how_many ); */
+
+
+
+        $parts = explode(" ", $query);
+
+        $sql = "select * from producto where (";
+        $val = array();
+        $first = true;
+        foreach ($parts as $p) {
+            if ($first) {
+                $first = false;
+            } else {
+                $sql .= " and ";
+            }
+            $sql .= "  nombre_producto like ? ";
+            array_push($val, "%" . $p . "%");
+        }
+
+        $sql .= " or codigo_producto like ? ) limit 20 ";
+        array_push($val, "%" . $query . "%");
+
+        global $conn;
+        $rs = $conn->Execute($sql, $val);
+        $ar = array();
+        foreach ($rs as $foo) {
+            $bar = new Producto($foo);
+            array_push($ar, $bar);
+        }
+        return $ar;
+    }
+
+    public static function ExistenciasTotales($id_producto) {
+
+        $lotes = LoteProductoDAO::search(new LoteProducto(array("id_producto" => $id_producto)));
+
+        $total = 0;
+
+        foreach ($lotes as $l) {
+            $total += $l->getCantidad();
+        }
+
+        return $total;
+    }    
+
+    public static function buscarProductoEnSucursal($id_producto, $id_sucursal) {
+        $sql = "SELECT
 				lp.cantidad,
 				lp.id_unidad,
 				lp.id_lote,
@@ -89,13 +86,14 @@ class ProductoDAO extends ProductoDAOBase
 				and lp.id_lote = l.id_lote
 				and l.id_almacen = a.id_almacen
 				and a.id_sucursal = ?";
-				
-			global $conn;
-			$rs = $conn->Execute($sql, array($id_producto, $id_sucursal));
-			$ar = array( );
-			foreach ($rs as $foo) {
-	    		array_push( $ar, $foo);
-			}
-			return $ar;
-	}
+
+        global $conn;
+        $rs = $conn->Execute($sql, array($id_producto, $id_sucursal));
+        $ar = array();
+        foreach ($rs as $foo) {
+            array_push($ar, $foo);
+        }
+        return $ar;
+    }
+
 }
