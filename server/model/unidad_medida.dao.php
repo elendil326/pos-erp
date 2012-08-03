4c809ca8813ea8ac2946c8f6ce91b2221ab9c0f1
Juan Manuel Garcia <zonademanel@gmail.com>
2012-08-03 16:47:27 +0000

    fix metodo de conversion de unidades

--- server/model/unidad_medida.dao.php
+++ server/model/unidad_medida.dao.php
@@ -21,27 +21,28 @@ require_once("base/unidad_medida.vo.base.php");
 class UnidadMedidaDAO extends UnidadMedidaDAOBase
 {
 public static function convertir( $id_unidad_desde,  $id_unidad_destino,  $cantidad_desde )
-  {
-
-     Logger::log("<<<<<<<<<<se convertira ( {$id_unidad_desde}, {$id_unidad_destino}, {$cantidad_desde} )>>>>>>>>>>>>>");
+  {     
+    
+      Logger::log("<<<<<<<<<< se rebibieron  ( {$id_unidad_desde}, {$id_unidad_destino}, {$cantidad_desde} ) >>>>>>>>>>>>>");
     
       //buscar esas unidades
-      $uFrom = self::getByPK( $id_unidad_desde );
-      $uTo = self::getByPK( $id_unidad_destino );
+      $x = self::getByPK( $id_unidad_desde );
+      $y = self::getByPK( $id_unidad_destino );
+      $c = $cantidad_desde;
 
-      if(is_null( $uFrom)) throw new InvalidDataException("La unidad no existe (desde)");
-      if(is_null( $uTo)) throw new InvalidDataException("La unidad no existe (destino)");
+      if(is_null( $x)) throw new InvalidDataException("La unidad no existe (desde)");
+      if(is_null( $y)) throw new InvalidDataException("La unidad no existe (destino)");
 
 
       //verifiquemos que las dos unidades sean de la misma categoria
-      if($uFrom->getIdCategoriaUnidadMedida() != $uTo->getIdCategoriaUnidadMedida()){
+      if($x->getIdCategoriaUnidadMedida() != $y->getIdCategoriaUnidadMedida()){
           Logger::error("En UnidadMedidaDAO::convertir(id_unidad_desde $id_unidad_desde, id_unidad_destino $id_unidad_destino, cantidad_desde $cantidad_desde)");
           Logger::error("No se pueden convertir entre diferentes categorias");
           throw new BusinessLogicException("No se pueden convertir entre diferentes categorias");
       }
 
 
-
+      /*
       //busquemos que esa categoria tena una referencia
       $uRef = self::search( new UnidadMedida( array( 
             "id_categoria_unidad_medida" => $uTo->getIdCategoriaUnidadMedida(),
@@ -58,10 +59,24 @@ public static function convertir( $id_unidad_desde,  $id_unidad_destino,  $canti
       $intermediate = $cantidad_desde * $uRef->getFactorConversion();
 
       //convertir a destino
-      $result = $intermediate / $uTo->getFactorConversion();
+      $result = $intermediate / $uTo->getFactorConversion();*/
+      
+      /**
+       * ALGORITMO
+       * 
+       * Se recibe en la funcion :
+       * convertir (x, y, c)
+       * 
+       * Formula : Factor(x) * c / Factor(y)
+       * 
+       */
+      
+      
 
       //regresar
-      return $result;
+      $r = ($x->getFactorConversion() * $c / $y->getFactorConversion());
+      Logger::log("<<<<<<<<<< se transformaron {$cantidad_desde} " . UnidadMedidaDAO::getByPK($id_unidad_desde)->getAbreviacion() .  " a {$r}  " . UnidadMedidaDAO::getByPK($id_unidad_destino)->getAbreviacion() .  " >>>>>>>>>>>>>");
+      return $r;
 
   }
 }
