18875394a00df496958e827e580a59109ba76e5e
Alan Gonzalez <alanboy@alanboy.net>
2013-04-14 09:39:27 +0000

    php sdk cleanup

--- sdk/php/pos-erp-sdk.php
+++ sdk/php/pos-erp-sdk.php
@@ -1,8 +1,6 @@
 <?php
 
-
 Class PosErp{
-
     private static $singleton_obj;
     private $waiting;
     private $instance_token;
@@ -18,73 +16,53 @@ Class PosErp{
         $this->at = null;
     }
 
-
-
     function __clone(){
-
-
     }
-
-
 
     function __destroy(){
-
-
     }
 
-
-
-
     public static function getInstance( $i_token = null ){
-
         if(self::$singleton_obj === null){
-
             if($i_token === null){
                 throw new Exception("You must pass your instance token when calling getInstance() for the first time.");
             }
-
             self::$singleton_obj = new PosErp($i_token);
         }
-
         return self::$singleton_obj;  
     }
 
-
-
-
     private function postRequest($url, $data, $referer='') {
-     
         // Convert the data array into URL Parameters like a=b&foo=bar etc.
         $data = http_build_query($data);
-     
+
         // parse the given URL
         $url = parse_url($url);
-     
+
         if ($url['scheme'] != 'http') { 
             die('Error: Only HTTP request are supported !');
         }
-     
+
         // extract host and path:
         $host = $url['host'];
         $path = $url['path'];
-     
+
         // open a socket connection on port 80 - timeout: 30 sec
         $fp = fsockopen($host, 80, $errno, $errstr, 30);
-     
+
         if ($fp){
-     
             // send the request headers:
             fputs($fp, "POST $path HTTP/1.1\r\n");
             fputs($fp, "Host: $host\r\n");
-     
+
             if ($referer != '')
                 fputs($fp, "Referer: $referer\r\n");
-     
+
             fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
             fputs($fp, "Content-length: ". strlen($data) ."\r\n");
             fputs($fp, "Connection: close\r\n\r\n");
             fputs($fp, $data);
-     
+
             $result = ''; 
             while(!feof($fp)) {
                 // receive the results of the request
@@ -97,16 +75,16 @@ Class PosErp{
                 'error' => "$errstr ($errno)"
             );
         }
-     
+
         // close the socket connection:
         fclose($fp);
-     
+
         // split the result header from the content
         $result = explode("\r\n\r\n", $result, 2);
-     
+
         $header = isset($result[0]) ? $result[0] : '';
         $content = isset($result[1]) ? $result[1] : '';
-     
+
         // return as structured array:
         return array(
             'status' => 'ok',
@@ -114,10 +92,6 @@ Class PosErp{
             'content' => $content
         );
     }
-
-    
-
-
 
     private function BeforeCall( $api_name, $toSendParams ){
         if($api_name == "api/sesion/iniciar"){
@@ -135,18 +109,12 @@ Class PosErp{
                 $toSendParams["at"] = $this->at; 
 
             }
-            
-            
         }
 
         return $toSendParams;
     }
 
-
-
-
     private function AfterCall( $result ){
-
         //decode json
         $response = json_decode($result);
 
@@ -155,36 +123,23 @@ Class PosErp{
                 $this->at = $response->auth_token;
             }
         }
-        
+
         $this->incoming_login_results = false;
-
         return $response;
-
     }
 
-
-
-
-
     private function ApiCall($method, $address, $params ){
-
         $params = $this->BeforeCall($address, $params );
 
         if(is_null($this->instance_token)){
             throw new Exception("You have not supplied any instance token", 1);
         }
 
-
-
         //$this->BeforeCall();
-
-
-
 
         if($method === "POST"){
            $r = $this->postRequest($this->url .  $this->instance_token . "/" . $address , $params );
             return $this->AfterCall( $r['content'] );
-
 
         }else if($method === "GET"){
             //convert params to ?x=y& format
@@ -195,51 +150,35 @@ Class PosErp{
 
         }else{
             throw new Exception("Http methdos must be POST or GET", 1);
-            
 
         }
-
-        
     }
-
-
 
     public function POST($api_name, $parameters){
         return $this->ApiCall( "POST", $api_name, $parameters );
     }
 
-
     public function GET($api_name, $parameters){
         return $this->ApiCall( "GET", $api_name, $parameters );
     }
-
-
 }
-
-
-
 
 
 
 /*
 
-
+   Example
 
 $api = PosErp::getInstance("1e65da5dbe04139ee8d810568f1fd406");
-
 
 $api->POST("api/sesion/iniciar", array( 
         'password' => '123',
         'usuario' => '1'
      ));
 
-
 $api->POST("api/cliente/nuevo", array( 
-        'razon_social' => 'Alan Gonzalez'
+        'razon_social' => 'Laura Gonzalez'
      ));
 
-
-
-exit;
-
 */
+
