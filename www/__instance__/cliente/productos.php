4fcac5c13fd250ef49d110831516da05f6df8bcf
Mauricio Nunez <themjnunez@gmail.com>
2013-04-19 21:59:47 +0000

    Bug 707 solucionado

--- www/__instance__/cliente/productos.php
+++ www/__instance__/cliente/productos.php
@@ -7,18 +7,40 @@ $page->nextTab("Lista");
 
 $campos = ConfiguracionDAO::Propiedades();
 $columns = array();
-foreach ($campos as $value)
-{
+foreach ($campos as $value) {
     $columns[$value] = ucwords(str_replace('_', ' ', $value));
 }
-$products = ProductosController::Lista();
+
+$products = array_filter(ProductosController::Lista(), "filter");
+
+foreach ($products as $product) {
+	$product->setPrecio(FormatMoney($product->getPrecio()));
+	$product->setCostoEstandar(FormatMoney($product->getCostoEstandar()));
+	$product->setCostoExtraAlmacen(FormatMoney($product->getCostoExtraAlmacen()));
+
+	if ($product->getCompraEnMostrador()) {
+		$product->setCompraEnMostrador("S&iacute;");
+	} else {
+		$product->setCompraEnMostrador("No");
+	}
+
+	$unidad = UnidadMedidaDAO::getByPK($product->getIdUnidadCompra());
+	if (!is_null($unidad)) {
+		$product->setIdUnidadCompra($unidad->getDescripcion());
+	}
+
+	$unidad = UnidadMedidaDAO::getByPK($product->getIdUnidad());
+	if (!is_null($unidad)) {
+		$product->setIdUnidad($unidad->getDescripcion());
+	}
+}
 
 function filter($product)
 {
     return $product->getVisibleEnVc();
 }
 
-$table = new TableComponent($columns, array_filter($products, "filter"));
+$table = new TableComponent($columns, $products);
 $table->addOnClick("id_producto", "(function(a) {window.location = 'productos.ver.php?pid=' + a;})");
 
 $page->addComponent($table);
