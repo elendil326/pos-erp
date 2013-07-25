b60de8fd8894ca0899291c424a42e359fb1f0ec4
Alan Gonzalez <alanboy@alanboy.net>
2013-07-25 21:11:35 +0000

    Removed wrong testcase

--- tests/phpunit/EmpresasControllerTest.php
+++ tests/phpunit/EmpresasControllerTest.php
@@ -1,10 +1,8 @@
 <?php
-	//ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . __DIR__ );
 	require_once("../../server/bootstrap.php");
 	
 class EmpresasControllerTest extends PHPUnit_Framework_TestCase {
-	
-	
+
 	private $_empresa;
 
 	protected function setUp(){
@@ -16,25 +14,23 @@ class EmpresasControllerTest extends PHPUnit_Framework_TestCase {
 			global $POS_CONFIG;
 			$POS_CONFIG["INSTANCE_CONN"]->Execute("INSERT INTO `usuario` (`id_usuario`, `id_direccion`, `id_direccion_alterna`, `id_sucursal`, `id_rol`, `id_clasificacion_cliente`, `id_clasificacion_proveedor`, `id_moneda`, `fecha_asignacion_rol`, `nombre`, `rfc`, `curp`, `comision_ventas`, `telefono_personal1`, `telefono_personal2`, `fecha_alta`, `fecha_baja`, `activo`, `limite_credito`, `descuento`, `password`, `last_login`, `consignatario`, `salario`, `correo_electronico`, `pagina_web`, `saldo_del_ejercicio`, `ventas_a_credito`, `representante_legal`, `facturar_a_terceros`, `dia_de_pago`, `mensajeria`, `intereses_moratorios`, `denominacion_comercial`, `dias_de_credito`, `cuenta_de_mensajeria`, `dia_de_revision`, `codigo_usuario`, `dias_de_embarque`, `tiempo_entrega`, `cuenta_bancaria`, `id_tarifa_compra`, `tarifa_compra_obtenida`, `id_tarifa_venta`, `tarifa_venta_obtenida`) VALUES
 				(1, NULL, NULL, NULL, 0, NULL, NULL, NULL, '2011-10-24 18:28:24', 'Administrador', NULL, NULL, NULL, NULL, NULL, '2011-10-24 18:28:34', NULL, 1, 0, NULL, '202cb962ac59075b964b07152d234b70', NULL, 0, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, NULL, 0, 'rol', 0, 'rol');");
-				
+
 			$r = SesionController::Iniciar(123, 1, true);
 		}
-
-
-		
-
 	}
-	
 
-	
-	public function testBootstrap(){
-		
+	public static function setUpBeforeClass() {
+		POSController::DropBd();
+	}
+
+	public function testBootstrap() {
+
 		$r = SesionController::Iniciar(123, 1, true);
 
-	 	$this->assertEquals($r["login_succesful"], true);
-	
+		$this->assertEquals($r["login_succesful"], true);
+
 		$r = SesionController::getCurrentUser(  );
-		
+
 		$this->assertEquals($r->getIdUsuario(), 1);
 	}
 
@@ -42,28 +38,25 @@ class EmpresasControllerTest extends PHPUnit_Framework_TestCase {
 
 		$direccion = Array(
 			"calle"  			=> "Monte Balcanes",
-	        "numero_exterior"   => "107",
-	        "colonia"  			=> "Arboledas",
-	        "id_ciudad"  		=> 334,
-	        "codigo_postal"  	=> "38060",
-	        "numero_interior"  	=> null,
-	        "texto_extra"  		=> "Calle cerrada",
-	        "telefono1"  		=> "4616149974",
-	        "telefono2"			=> "45*451*454"
+			"numero_exterior"   => "107",
+			"colonia"  			=> "Arboledas",
+			"id_ciudad"  		=> 334,
+			"codigo_postal"  	=> "38060",
+			"numero_interior"  	=> null,
+			"texto_extra"  		=> "Calle cerrada",
+			"telefono1"  		=> "4616149974",
+			"telefono2"			=> "45*451*454"
 		);
-		
+
 		$id_moneda = 1;
 		$razon_social = "Caffeina Software".time();
 		$rfc  = "GOHA".time();
-		
 
-		
 		$empresas_con_rfc = EmpresaDAO::getByRFC($rfc);
-		
+
 		foreach ($empresas_con_rfc as $empObj) {
 			EmpresasController::Eliminar( $empObj->getIdEmpresa() );
 		}
-
 
 		$this->_empresa = EmpresasController::Nuevo(
 				$direccion 			= array( $direccion ), 
@@ -78,24 +71,15 @@ class EmpresasControllerTest extends PHPUnit_Framework_TestCase {
 				$representante_legal = time()	."f", 
 				$sucursales 		= null, 
 				$texto_extra 		= time()	."g"
-
 			);
 
 		$this->assertInternalType('int', $this->_empresa["id_empresa"] );
-		
+
 		$dao = EmpresaDAO::getByPK($this->_empresa["id_empresa"]);
-		
+
 		$this->assertEquals($razon_social, 			$dao->getRazonSocial());
 		$this->assertEquals($rfc, 					$dao->getRfc());
 		$this->assertEquals($representante_legal,	$dao->getRepresentanteLegal());
-
-		
-
-		/*$this->assertInternalType('int', $dao->getIdDireccion() );	*/
-		
-		//traer la direccion para comparar valores
-		
-		
 	}
 
 	/**
@@ -206,20 +190,6 @@ class EmpresasControllerTest extends PHPUnit_Framework_TestCase {
 		$this->assertGreaterThan( 0, $busqueda["numero_de_resultados"]);
 		$this->assertInternalType('int', $busqueda["numero_de_resultados"]);
 		$this->assertEquals( $busqueda["numero_de_resultados"], sizeof( $busqueda["resultados"]));
-	}
-	
-
-
-	
-	public function testBuscarWithParams(){
-		$busqueda = EmpresasController::Buscar( true );
-		//solo debe haber empresas activas
-		$res = $busqueda["resultados"];
-		
-		foreach ($res as $emp) {
-			$this->assertEquals(1, $emp->getActivo());
-		}
-		
 	}
 	
 
