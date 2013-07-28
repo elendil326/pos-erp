a8f110fff036936ce0fb51a0e519da21632da7ef
Alan Gonzalez <alanboy@alanboy.net>
2013-07-28 06:43:39 +0000

    BUG:729 - Al crear un nuevo impuesto hay un error 500

--- server/controllers/Impuestos.controller.php
+++ server/controllers/Impuestos.controller.php
@@ -7,28 +7,7 @@ require_once("interfaces/Impuestos.interface.php");
   **/
 	
   class ImpuestosController implements IImpuestos{
-  
-	
-	/**
-	*
-	*Crear un nuevo impuesto. Falta revisar bien lo de contabilidad, para saber como se van a ligar los impuestos con las cuentas, pero omitiendo las ligas con las cuentas seria esto.
-	*
- 	 * @param codigo string Cdigo del impuesto
- 	 * @param importe float Para impuestos de tipo porcentaje, introdusca valor % entre 0-1
- 	 * @param nombre string Nombre del impuesto
- 	 * @param descripcion string Puede usarse para poner la descripcin de un impuesto o un cdigo. 
- 	 * @param secuencia int Es usado para ordenar las lineas de impuestos de menor a mayor secuencia. El orden es importante si un impuesto tiene varios impuestos hijos. En este caso, el orden de evaluacin es importante. 
- 	 * @param tipo int El metodo de calculo del importe del impuesto. Porcentaje (0), Importe fijo (1), ninguno (2), saldo pendiente (3)
- 	 * @return id_impuesto int Id del impuesto insertado.
- 	 **/
-	/*public static function Nuevo
-	(
-		$monto_porcentaje, 
-		$nombre, 
-		$tipo = ""
-	)*/
-      
-      
+
       /**
  	 *
  	 *Crear un nuevo impuesto. Falta revisar bien lo de contabilidad, para saber como se van a ligar los impuestos con las cuentas, pero omitiendo las ligas con las cuentas seria esto.
@@ -56,26 +35,27 @@ Para impuestos de tipo porcentaje, introduzca valor % entre 0-1
 		$tipo = ""
 	)
 	{  
-  		Logger::log("Nuevo impuesto...");
 
-		if($monto_porcentaje > 1){
-			throw new InvalidDataException( "Porcentaje debe expresarse en un rango de 0 a 1" );
-		}
 
 		$ni = new Impuesto();
-		$ni->setMontoPorcentaje( $monto_porcentaje );
-		$ni->setEsMonto( 0 );
+		$ni->setImporte( $importe );
 		$ni->setNombre( $nombre );
-		$ni->setDescripcion( $nombre );
-  
+		$ni->setCodigo( $codigo );
+		$ni->setIncluidoPrecio( $incluido_precio );
+		$ni->setAplica( $aplica );
+		$ni->setTipo( $tipo );
+		$ni->setNombre( $nombre );
+		$ni->setActivo( $activo );
+		$ni->setDescripcion( $descripcion );
+
 		try{
 			ImpuestoDAO::save($ni);
 				
 		}catch(Exception $e){
-			throw new InvalidDatabaseException($e);
-			
+			throw new InvalidDatabaseOperationException($e);
 		}
 
+  		Logger::log("Nuevo impuesto..." . $ni->getIdImpuesto() . " creado" );
 		return array( "id_impuesto" => $ni->getIdImpuesto() );
 	}
 	
